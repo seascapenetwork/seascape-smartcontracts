9ef53918ee4cb852a6819a7778f4f9f0894568a7
Medet Ahmetson <admin@blocklords.io>
2020-10-17 13:57:07 +0700

    Add a development network config

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,15 @@
 module.exports = {
-   compilers: {
-    solc: {
-      version: "0.6.7"
+    compilers: {
+	solc: {
+	    version: "0.6.7"
+	}
+    },
+    networks: {
+       development: {
+	   host: "blockchain",
+	   port: 8545,
+	   network_id: "*", // match any network
+	   from: process.env.ADDRESS_1
+       },
     }
-  }
 };
