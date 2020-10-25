53adbf87ce071a4faa8dfa226c23a96a835125f6
Medet Ahmetson <admin@blocklords.io>
2020-10-25 11:21:27 +0700

    Use Crowns for Rinkeby in migration if deployment is on rinkeby

--- migrations/2_staking_migration.js
+++ migrations/2_staking_migration.js
@@ -3,9 +3,16 @@ if (process.env.STAKING !== undefined) {
     Staking.address = process.env.STAKING
 }
 
-module.exports = function(deployer) {
-    deployer.deploy(Staking, process.env.CROWNS).then(function(){
-	console.log("Staking contract was deployed at address: "+Staking.address);
-	console.log("It is used with Crowns (CWS) Token at address: "+process.env.CROWNS);
-    });
+module.exports = function(deployer, network) {
+    if (network == "development") {
+	deployer.deploy(Staking, process.env.CROWNS).then(function(){
+	    console.log("Staking contract was deployed at address: "+Staking.address);
+	    console.log("It is used with Crowns (CWS) Token at address: "+process.env.CROWNS);
+	});
+    } else if (network == "rinkeby") {
+        deployer.deploy(Staking, process.env.CROWNS_RINKEBY).then(function(){
+	    console.log("Staking contract was deployed at address: "+Staking.address);
+	    console.log("It is used with Crowns (CWS) Token at address: "+process.env.CROWNS);
+	});
+    }
 };
