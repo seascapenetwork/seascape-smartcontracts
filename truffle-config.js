aad5a788fd9bd4b12ce3e7da35a8c90acc9f5fd7
Medet Ahmetson <admin@blocklords.io>
2020-10-25 11:20:28 +0700

    Add a rinkeby deployment config

--- truffle-config.js
+++ truffle-config.js
@@ -12,6 +12,14 @@ module.exports = {
 	   port: 8545,
 	   network_id: "*", // match any network
 	   from: process.env.ADDRESS_1
-       },
+        },
+	rinkeby: {
+	    provider: function() { 
+		return new HDWalletProvider(process.env.MNEMONIC, process.env.HOST);
+	    },
+	    network_id: 4,
+	    skipDryRun: true // To prevent async issues occured on node v. 14. see:
+	    // https://github.com/trufflesuite/truffle/issues/3008
+	}
     }
 };
