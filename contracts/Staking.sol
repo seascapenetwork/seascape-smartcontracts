55585248156e88784087755dbc6426fa43f295fa
Medet Ahmetson <admin@blocklords.io>
2020-10-28 07:47:32 +0700

    Load Safe Math to Staking contract

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -3,6 +3,7 @@ pragma solidity 0.6.7;
 import "./openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "./openzeppelin/contracts/access/Ownable.sol";
+import "./openzeppelin/contracts/math/SafeMath.sol";
 import "./NFTFactory.sol";
 
 contract Staking is Ownable {
