23e766ff1f4cb31b911efcc6da615e24d1e115b5
Medet Ahmetson <admin@blocklords.io>
2020-10-28 07:49:16 +0700

    Load Safe Math library in NFT Factory

--- contracts/NFTFactory.sol
+++ contracts/NFTFactory.sol
@@ -1,6 +1,7 @@
 pragma solidity 0.6.7;
 
 import "./openzeppelin/contracts/access/Ownable.sol";
+import "./openzeppelin/contracts/math/SafeMath.sol";
 
 contract NFTFactory is Ownable {
     using SafeMath for uint256;
