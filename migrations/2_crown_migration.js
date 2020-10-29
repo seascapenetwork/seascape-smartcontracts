be34e3a530a08ce42991739b6373315cf9f40a91
Medet Ahmetson <admin@blocklords.io>
2020-10-29 17:48:17 +0700

    Migrate Crowns as a first contract (In development only)

---
+++ migrations/2_crown_migration.js
@@ -0,0 +1,9 @@
+var Crowns = artifacts.require("./CrownsToken.sol");
+
+module.exports = function(deployer, network) {
+    if (network == "development") {
+	deployer.deploy(Crowns).then(function(){
+	    console.log("Crowns Test token contract was deployed at address: "+Crowns.address);
+	});
+    }
+};
