203721faa1ee59da45301ef68a2284317a9b6f9d
Medet Ahmetson <admin@blocklords.io>
2020-10-29 18:31:55 +0700

    LP CWS Test tokens are minted to contract deployer

--- contracts/LP_Token.sol
+++ contracts/LP_Token.sol
@@ -21,7 +21,7 @@ contract LP_Token is ERC20, Ownable {
     constructor() public ERC20("LP CWS Test", "LPT") {
         uint256 supply        = (10 * _million * (10 ** 18));
    
-	address newOwner      = 0xC6EF8A96F20d50E347eD9a1C84142D02b1EFedc0;
+	address newOwner      = msg.sender;
 
         _mint(newOwner,       supply);
 
