0933a140e91cca1960a309cc29248f02a09ed394
Medet Ahmetson <admin@blocklords.io>
2020-10-31 02:02:07 +0700

    Deploy Uniswap V2 LP Token Test in development only

--- migrations/3_lp_test_migration.js
+++ migrations/3_lp_test_migration.js
@@ -1,8 +1,10 @@
 var LPToken = artifacts.require("./LP_Token.sol");
 
 module.exports = function(deployer, network) {
+    if (network == "development") {
 	deployer.deploy(LPToken).then(function(){
 	    console.log("LP Test token contract was deployed at address: "+LPToken.address);
 	});
-
+    }
 };
+
