17e0834313b975ccc6eb6a23a1403d92d89b376a
Medet Ahmetson <admin@blocklords.io>
2020-11-14 14:17:27 +0700

    Add sample message signing in claim script

--- scripts/nft_rush/claim.js
+++ scripts/nft_rush/claim.js
@@ -3,6 +3,7 @@ let LpToken = artifacts.require("LP_Token");
 let Crowns = artifacts.require("CrownsToken");
 let Nft = artifacts.require("SeascapeNFT");
 let Factory = artifacts.require("NFTFactory");
+let Staking = artifacts.require("Staking");
 
 var accounts;
 
@@ -21,18 +22,28 @@ let init = async function() {
     let nftRush = await NftRush.deployed();
     
     let lastSessionId = await nftRush.lastSessionId();
+
+    let nft = await Nft.deployed();
+    let balance = await nft.balanceOf(accounts[0]);
+
+    let crowns = await Crowns.deployed();
+    let factory = await Factory.deployed();
+    let staking = await Staking.deployed();
+
+    
     return await claimNft(nftRush, lastSessionId);
 }.bind(this);
 
 //should claim random nft
 let claimNft = async function(nftRush, lastSessionId) {
     let quality = getRandomInt(5) + 1;
+
     let addr = accounts[0];
     
     let balance = await nftRush.balances(lastSessionId, addr);
     let amountWei = web3.utils.toWei(web3.utils.fromWei(balance.amount));
     let mintedTime = parseInt(balance.mintedTime.toString());
-	
+
     let bytes32 = web3.eth.abi.encodeParameters(["uint256", "uint256"],
 						[amountWei, mintedTime]);
     
@@ -48,8 +59,29 @@ let claimNft = async function(nftRush, lastSessionId) {
     }
 
     let res = await nftRush.claim(lastSessionId, v, r, s, quality);
+    console.log(res);
     return res;
 }.bind(this);
+
+let sampleMsgSigning = async function() {
+    let addr = accounts[0];
+
+    let raw256_1 = 12345;
+    let raw256_2 = 54321;
+    let bytes32 = web3.eth.abi.encodeParameters(["uint256", "uint256"],
+						[raw256_1, raw256_2]);
+    let quality = 3;
+    let bytes1  = web3.utils.bytesToHex([quality]);
+
+    let keccak = web3.utils.keccak256(bytes1);
+    let signature = await web3.eth.sign(data, addr);
+
+    console.log(addr + " is signing:");
+    console.log("Quality:    "+quality);
+    console.log("as hex:     "+bytes1);
+    console.log("hashed:     "+keccak);
+    console.log("signature:  "+signature;
+};
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
