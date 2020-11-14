3ced7763d05be7ef0bd8733ebe886c59705f2698
Medet Ahmetson <admin@blocklords.io>
2020-11-14 14:16:38 +0700

    Crowns token migration in all networks

--- migrations/4_staking_migration.js
+++ migrations/4_staking_migration.js
@@ -1,8 +1,6 @@
 var Staking = artifacts.require("./Staking.sol");
 var Crowns = artifacts.require("./CrownsToken.sol");
-if (process.env.STAKING !== undefined) {
-    Staking.address = process.env.STAKING
-}
+
 
 module.exports = function(deployer, network) {
     if (network == "development") {
@@ -11,9 +9,9 @@ module.exports = function(deployer, network) {
 	    console.log("It is used with Crowns (CWS) Token at address: "+Crowns.address);
 	});
     } else if (network == "rinkeby") {
-        deployer.deploy(Staking, process.env.CROWNS_RINKEBY).then(function(){
+        deployer.deploy(Staking, Crowns.address).then(function(){
 	    console.log("Staking contract was deployed at address: "+Staking.address);
-	    console.log("It is used with Crowns (CWS) Token at address: "+process.env.CROWNS);
+	    console.log("It is used with Crowns (CWS) Token at address: "+Crowns.address);
 	});
     }
 };
