98765703c560439afd3972cf32206dadd1c133a7
Medet Ahmetson <admin@blocklords.io>
2020-12-15 19:31:07 +0700

    add a leaderboard prize set during nft rush game initiation

--- scripts/nft_rush/init.js
+++ scripts/nft_rush/init.js
@@ -7,6 +7,8 @@ let accounts;
 let interval = 10;  // seconds
 let period = 3600 * 60 * 5;   // 1 week 
 let generation = 0;
+let rewardPrize = 10; // first winner gets 10 CWS
+let winnersAmount = 10; // ten winners are tracked
 
 /**
  * For test purpose, starts a game session
@@ -32,7 +34,11 @@ let init = async function(networkId) {
 	crowns = await Crowns.at(process.env.CROWNS_RINKEBY);
     } else {
 	crowns = await Crowns.deployed();
-    }*/
+	}*/
+
+    console.log("Total prize to pay: "+calculateTotalPrize());
+
+    await setAllRewards(nftRush);
         
     //should add nft rush as generator role in nft factory
     await factory.addGenerator(nftRush.address, {from: accounts[0]});
@@ -41,6 +47,7 @@ let init = async function(networkId) {
     //should set nft factory in nft
     await nft.setFactory(factory.address);
 
+    
     //should start a session
     let startTime = Math.floor(Date.now()/1000) + 1;
     return await nftRush.startSession(interval,
@@ -55,6 +62,30 @@ let init = async function(networkId) {
 // ------------------------------------------------------------
 // Leaderboard related data
 // ------------------------------------------------------------
+let setAllRewards = async function(nftRush) {
+    let winners = [];
+    for(var i=1; i<=winnersAmount; i++) {
+	let amount = Math.round(rewardPrize / i);
+	let wei = web3.utils.toWei(amount.toString());
+	winners.push(wei);
+    }
+
+    await nftRush.setAllRewards(winners, winners, winners, winners);
+
+    console.log("Set all reward prizes");
+};
+
+let calculateTotalPrize = function() {
+    let total = 0;
+    for(var i=1; i<=winnersAmount; i++) {
+	let amount = Math.round(rewardPrize / i);
+	total += amount;
+	console.log(total+" for "+i+" users");
+    }
+
+    return total;
+};
+
 let addDailyWinners = async function(nftRush, lastSessionId) {
     // in nftrush.sol contract, at the method claimDailyNft
     // comment requirement of isDailWinnersAddress against false checking
