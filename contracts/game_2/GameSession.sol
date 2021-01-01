6e77d5d19d4e70e101d3ad16b3395732002fe258
Medet Ahmetson <ahmetson@zoho.com>
2021-01-01 20:42:54 +0700

    Refactoring Game session contract to make it production ready

--- contracts/game_2/GameSession.sol
+++ contracts/game_2/GameSession.sol
@@ -4,14 +4,18 @@ import "./../openzeppelin/contracts/access/Ownable.sol";
 import "./../openzeppelin/contracts/math/SafeMath.sol";
 import "./../openzeppelin/contracts/utils/Counters.sol";
 
+/// @notice Nft Rush and Leaderboard contracts both requires Game Session data
+/// So, making Game Session separated.
+///
+/// Game session indicates activity of Game for certain period of time.
+///
+/// @author Medet Ahmetson
 contract GameSession is Ownable {
     using SafeMath for uint256;
     using Counters for Counters.Counter;
 
     Counters.Counter private sessionId;
 
-    /// @notice Game session. Smartcontract is active during the game session.
-    /// Game session is active for a certain period of time only
     struct Session {
 	uint256 interval;      // period between intervals
 	uint256 period;        // duration of session
@@ -19,51 +23,64 @@ contract GameSession is Ownable {
 	uint256 generation;    // nft generation
     }
 
-    uint256 public lastSessionId;
+    /// @notice Game session. Smartcontract is active during the game session.
+    /// Game session is active for a certain period of time only
     mapping(uint256 => Session) public sessions;
 
     event SessionStarted(uint256 id, uint256 startTime, uint256 endTime, uint256 generation);
-	
-    constructor() public {
-	// Starts at value 1. 
-	sessionId.increment();
-    }
-
     
     //--------------------------------------------------
     // Only owner
     //--------------------------------------------------
     
-    /// @notice Starts a staking session for a finit _period of
-    /// time, starting from _startTime. The _totalReward of
-    /// CWS tokens will be distributed in every second. It allows to claim a
-    /// a _generation Seascape NFT.
-    /// @param _interval duration between claims
-    /// @param _period session duration
-    /// @param _startTime session start time in unix timestamp
-    /// @param _generation Seascape Nft generation that is given as a reward
+    /**
+     *  @notice Starts a staking session for a finit _period of
+     *  time, starting from _startTime. The _totalReward of
+     *  CWS tokens will be distributed in every second. It allows to claim a
+     *  a _generation Seascape NFT.
+     *
+     *  Emits a {SessionStarted} event.
+     *
+     *  @param _interval duration between claims
+     *  @param _period session duration
+     *  @param _startTime session start time in unix timestamp
+     *  @param _generation Seascape Nft generation that is given as a reward
+     */
     function _startSession(uint256 _interval, uint256 _period, uint256 _startTime, uint256 _generation) internal onlyOwner returns(uint256) {
 
+	sessionId.increment();		
 	uint256 _sessionId = sessionId.current();
-
+	
 	sessions[_sessionId] = Session(_interval, _period, _startTime, _generation);
 
-	sessionId.increment();
-	lastSessionId = _sessionId;
-
+	emit SessionStarted(_sessionId, _startTime, _startTime.add(_period), _generation);
+	
 	return _sessionId;
     }
 
     //--------------------------------------------------
-    // Private methods
+    // Public methods
     //--------------------------------------------------
 
-    
-    function isStartedFor(uint256 _sessionId) internal view returns(bool) {
+
+    /**
+     *  @notice Whether the given session is active or not
+     */
+    function isActive(uint256 _sessionId) public view returns(bool) {
 	if (now > sessions[_sessionId].startTime + sessions[_sessionId].period) {
 	    return false;
 	}
 
 	return true;
+    }
+
+
+    /**
+     * @notice The last created session's id
+     *
+     * NOTE!!! It returns 0, if no session was started yet.
+     */
+    function lastSessionId() public view returns(uint256) {
+	return sessionId.current();
     }
 }
