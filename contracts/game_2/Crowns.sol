02bf5452ed6fbd9a38c26c7f90e24393de118e1b
Medet Ahmetson <ahmetson@zoho.com>
2021-01-01 20:43:52 +0700

    add a comment to Nft Rush/Crowns contract explaining what it does

--- contracts/game_2/Crowns.sol
+++ contracts/game_2/Crowns.sol
@@ -2,6 +2,10 @@ pragma solidity 0.6.7;
 
 import "./../crowns/erc-20/contracts/CrownsToken/CrownsToken.sol";
 
+/// @notice Nft Rush and Leaderboard contracts both manipulates with Crowns.
+/// So, making Crowns available for both Contracts
+///
+/// @author Medet Ahmetson
 contract Crowns {
     CrownsToken public crowns;
 
