dc2ce7006812959eda2d63e74b2e4447f17a9175
Medet Ahmetson <ahmetson@zoho.com>
2021-01-16 12:37:33 +0700

    use gas price and value in deploying Nft Staking contract

--- migrations/8_nft_staking_migration.js
+++ migrations/8_nft_staking_migration.js
@@ -4,7 +4,7 @@ var Factory = artifacts.require("./NftFactory.sol");
 var Nft = artifacts.require("./SeascapeNft.sol");
 
 
-module.exports = function(deployer, network) {
+module.exports = async function(deployer, network) {
     if (network == "development") {
 	deployer.deploy(NftStaking, Crowns.address, Factory.address, Nft.address).then(function(){
 	    console.log("Staking contract was deployed at address: "+NftStaking.address);
@@ -13,11 +13,12 @@ module.exports = function(deployer, network) {
 
 	});
     } else if (network == "rinkeby") {
-        deployer.deploy(NftStaking, Crowns.address, Factory.address, Nft.address).then(function(){
-	    console.log("Staking contract was deployed at address: "+NftStaking.address);
-	    console.log("It is used with Crowns (CWS) Token at address: "+Crowns.address);
-	    console.log("To mint NFT it is using NFT Factory at address: "+Factory.address);
-	    console.log("Seascape Nft at  address: "+Nft.address);
+	let gasPrice = await web3.eth.getGasPrice();
+	let gasValue = 4700000;	    
+	
+        await deployer.deploy(NftStaking, "0x168840Df293413A930d3D40baB6e1Cd8F406719D", "0xF06CF016b6DAdED5f676EE6340fc7398CA2142b0", "0x7115ABcCa5f0702E177f172C1c14b3F686d6A63a",
+			{gas: gasValue, gasPrice: gasPrice}).then(function(){
+	    console.log("Nft Staking contract was deployed at address: "+NftStaking.address);
 	});
     }
 };
