ce5717d637321a012bb2b8173e9c86dfbe0d2757
Medet Ahmetson <ahmetson@zoho.com>
2021-02-02 18:51:43 +0700

    add initiation script for first game

--- scripts/lp_mining/init.js
+++ scripts/lp_mining/init.js
@@ -5,8 +5,8 @@ let Nft = artifacts.require("SeascapeNft");
 let Factory = artifacts.require("NftFactory");
 
 let accounts;
-let reward = web3.utils.toWei("200000", "ether");  // cws
-let period = 3600 * 24 * 60;   // 1 week
+let reward = web3.utils.toWei("1000", "ether");  // cws
+let period = 3600 * 2;
 let generation = 0;
 let lpTokenAddress = "0xdc935332d39a4c632864dbbed3cfdbf049fb9267";
 
@@ -23,12 +23,17 @@ module.exports = async function(callback) {
 let init = async function(networkId) {
     web3.eth.getAccounts(function(err,res) { accounts = res; });
 
-    let lpMining = await LpMining.deployed();
-    let factory = await Factory.deployed();
-    let nft     = await Nft.deployed();
+    let lpMining;
+    if (networkId != 4) {
+        lpMining = await LpMining.deployed();
+    } else {
+	lpMining = await LpMining.at("0x255C9ffD5EedEBD5bA99df58BB6C19872d3E8d0a");
+    }
+    let factory = await Factory.at("0xF06CF016b6DAdED5f676EE6340fc7398CA2142b0");
+    let nft     = await Nft.at("0x7115ABcCa5f0702E177f172C1c14b3F686d6A63a");
 
-    let crowns  = await Crowns.deployed();
-	
+    let crowns  = await Crowns.at("0x168840df293413a930d3d40bab6e1cd8f406719d");	
+
     if (networkId != 4) {
 	let lpToken = await LpToken.deployed();
 	lpTokenAddress = lpToken.address;
@@ -36,8 +41,8 @@ let init = async function(networkId) {
 
     let sessionId = await lpMining.lastSessionIds.call(lpTokenAddress);
     console.log(sessionId +" session id for "+lpTokenAddress);
-    console.log(lpMining.address);
-    return;
+    //console.log(lpMining.address);
+    //return;
     
     // should transfer reward amount to contract
     await crowns.transfer(lpMining.address, reward, {from: accounts[0]});
