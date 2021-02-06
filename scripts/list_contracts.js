6f12bed2c13cad0672816a55a7196795bb423930
Medet Ahmetson <ahmetson@zoho.com>
2021-02-06 12:25:50 +0700

    listing contract can also cancel the pending transactions

--- scripts/list_contracts.js
+++ scripts/list_contracts.js
@@ -6,24 +6,34 @@ let Factory = artifacts.require("NftFactory");
 let Staking = artifacts.require("LpMining");
 let NftStaking = artifacts.require("NftStaking");
 
-module.exports = function(callback) {
-    let res = init();    
+module.exports = async function(callback) {
+    //let res = await init();
+
+    //let accounts = await web3.eth.getAccounts();
+    //console.log(accounts);
+    //return;
+
+    // cancel pending transactions
+    console.log("canceling transactions...");
+    let res = await web3.eth.sendTransaction({from: accounts[0], to: accounts[0], value: 0, nonce: 3})
+    
+    console.log(res);
 };
 
 let init = async function() {
-    let nftRush = await NftRush.deployed();
+    //let nftRush = await NftRush.deployed();
     let crowns = await Crowns.deployed();
     let nft = await Nft.deployed();
     let factory = await Factory.deployed();
     let staking = await Staking.deployed();
-    let nftStaking = await NftStaking.deployed();
+    //let nftStaking = await NftStaking.deployed();
 
-    console.log("Nft Rush:    "+nftRush.address);
+    //console.log("Nft Rush:    "+nftRush.address);
     console.log("Crowns:      "+crowns.address);
     console.log("Nft:         "+nft.address);
     console.log("Factory:     "+factory.address);
     console.log("Lp Mining:   "+staking.address);
-    console.log("Nft Staking: "+nftStaking.address);
+    //console.log("Nft Staking: "+nftStaking.address);
     
     return true;
 }.bind(this);
