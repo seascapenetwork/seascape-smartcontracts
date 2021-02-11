99ad740ad583d850114ca8a9a560382a0657455b
Medet Ahmetson <admin@blocklords.io>
2021-02-12 06:17:24 +0700

    list two other game contracts in list_contracts script

--- scripts/list_contracts.js
+++ scripts/list_contracts.js
@@ -7,33 +7,34 @@ let Staking = artifacts.require("LpMining");
 let NftStaking = artifacts.require("NftStaking");
 
 module.exports = async function(callback) {
-    //let res = await init();
+    let res;
+    res = await init();
 
     //let accounts = await web3.eth.getAccounts();
     //console.log(accounts);
-    //return;
+    return;
 
     // cancel pending transactions
     console.log("canceling transactions...");
-    let res = await web3.eth.sendTransaction({from: accounts[0], to: accounts[0], value: 0, nonce: 3})
+    res = await web3.eth.sendTransaction({from: accounts[0], to: accounts[0], value: 0, nonce: 3})
     
     console.log(res);
 };
 
 let init = async function() {
-    //let nftRush = await NftRush.deployed();
+    let nftRush = await NftRush.deployed();
     let crowns = await Crowns.deployed();
     let nft = await Nft.deployed();
     let factory = await Factory.deployed();
     let staking = await Staking.deployed();
-    //let nftStaking = await NftStaking.deployed();
+    let nftStaking = await NftStaking.deployed();
 
-    //console.log("Nft Rush:    "+nftRush.address);
+    console.log("Nft Rush:    "+nftRush.address);
     console.log("Crowns:      "+crowns.address);
     console.log("Nft:         "+nft.address);
     console.log("Factory:     "+factory.address);
     console.log("Lp Mining:   "+staking.address);
-    //console.log("Nft Staking: "+nftStaking.address);
+    console.log("Nft Staking: "+nftStaking.address);
     
     return true;
 }.bind(this);
