30b4320324efb614437829f0525442cc53ed6013
ahmetson <admin@blocklords.io>
2021-03-16 21:45:45 +0700

    update listing of all contracts script

--- scripts/list_contracts.js
+++ scripts/list_contracts.js
@@ -22,19 +22,27 @@ module.exports = async function(callback) {
 };
 
 let init = async function() {
-    let nftRush = await NftRush.deployed();
     let crowns = await Crowns.deployed();
     let nft = await Nft.deployed();
     let factory = await Factory.deployed();
-    let staking = await Staking.deployed();
-    let nftStaking = await NftStaking.deployed();
 
-    console.log("Nft Rush:    "+nftRush.address);
     console.log("Crowns:      "+crowns.address);
     console.log("Nft:         "+nft.address);
     console.log("Factory:     "+factory.address);
+
+    // only in dev environment
+    let lpToken = await LpToken.deployed();
+    console.log("Lp Test:     "+lpToken.address);
+
+    /// Games
+    let staking = await Staking.deployed();
     console.log("Lp Mining:   "+staking.address);
-    console.log("Nft Staking: "+nftStaking.address);
+
+    let nftRush = await NftRush.deployed();
+    console.log("Nft Rush:    "+nftRush.address);
     
+    let nftStaking = await NftStaking.deployed();
+    console.log("Nft Staking: "+nftStaking.address);
+
     return true;
 }.bind(this);
