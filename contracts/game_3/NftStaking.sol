0086ee8751e05acd234277991bed54dc4db49a65
ahmetson <admin@blocklords.io>
2021-03-17 16:28:32 +0700

    For bonuses, use Nft Staking's owner account

--- contracts/game_3/NftStaking.sol
+++ contracts/game_3/NftStaking.sol
@@ -315,8 +315,9 @@ contract NftStaking is Ownable, IERC721Receiver {
         }
 
 	    uint256 totalBonus = _interests.mul(scaler).div(100).mul(_bonusPercent).div(scaler);
+        require(crowns.allowance(owner(), address(this)) >= totalBonus), "Seascape Staking: Not enough token to spend");
 
-        return crowns.transfer(msg.sender, totalBonus);
+        return crowns.transferFrom(address(this), msg.sender, totalBonus);
     }
 
     /// @dev earned CWS tokens are sent to Nft staker
