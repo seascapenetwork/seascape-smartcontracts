a5a012943d1edc568207777491058c59d857118f
ahmetson <admin@blocklords.io>
2021-04-10 21:30:58 +0700

    set the profit circus session starting script to work on rinkeby

--- scripts/lp_mining/init.js
+++ scripts/lp_mining/init.js
@@ -5,8 +5,8 @@ let Nft = artifacts.require("SeascapeNft");
 let Factory = artifacts.require("NftFactory");
 
 let accounts;
-let reward = web3.utils.toWei("1000", "ether");  // cws
-let period = 60;
+let reward = web3.utils.toWei("2000", "ether");  // cws
+let period = 2000;
 let generation = 1;
 
 /**
@@ -22,11 +22,11 @@ module.exports = async function(callback) {
 let init = async function(networkId) {
     web3.eth.getAccounts(function(err,res) { accounts = res; });
 
-	let lpMining = await LpMining.at("0x82b6ed562f202E76A5bDBB209e077f4a96bD5605");
+	let lpMining = await LpMining.at("0x8285dA17bF2a18369CC2a15e4F4Db68b9E7C0622");
 	//let factory = await Factory.at("0x3eB88c3F2A719369320D731FbaE062b0f82F22e4");
 	//let nft     = await Nft.at("0x66638F4970C2ae63773946906922c07a583b6069");
-	let crowns  = await Crowns.at("0xbcf39F0EDDa668C58371E519AF37CA705f2bFcbd");	
-    let lpTokenAddress = "0xe67b96a9456e6e623463d022fbb949a3fdd9ed4c";
+	let crowns  = await Crowns.at("0x168840Df293413A930d3D40baB6e1Cd8F406719D");	
+    let lpTokenAddress = "0xdc935332d39a4c632864dbbed3cfdbf049fb9267";
 
     //let nftGranted = await nft.setFactory(factory.address);
     //console.log("Nft has been linked to factory: "+nftGranted.tx);
@@ -35,8 +35,8 @@ let init = async function(networkId) {
     //console.log("2/2 Staking contract got a permission");
     
     // should transfer reward amount to contract
-    await crowns.transfer(lpMining.address, reward, {from: accounts[0]});
-    console.log("Crowns for session were transferred to the Lp Mining smartcontract");
+    //await crowns.transfer(lpMining.address, reward, {from: accounts[0]});
+    //console.log("Crowns for session were transferred to the Lp Mining smartcontract");
 
     let startTime = Math.floor(new Date().getTime()/1000) + 30;
     await lpMining.startSession(lpTokenAddress, reward, period, startTime, generation);
