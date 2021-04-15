847393873b6f27e8f399f2ec32195fa56b83133f
ahmetson <admin@blocklords.io>
2021-04-15 19:47:22 +0700

    Set correct name of projects in migration files for rinkeby network

--- migrations/6_lp_mining_migration.js
+++ migrations/6_lp_mining_migration.js
@@ -12,7 +12,7 @@ module.exports = function(deployer, network) {
 		});
     } else if (network == "rinkeby") {
         	deployer.deploy(LpMining, "0x168840df293413a930d3d40bab6e1cd8f406719d", "0xF06CF016b6DAdED5f676EE6340fc7398CA2142b0").then(function(){
-	    	console.log("Lp Mining contract was deployed at address: "+LpMining.address);
+	    	console.log("Profit Circus contract was deployed at address: "+LpMining.address);
 		});
     } else {
 		let crowns = "0x4Ca0ACab9f6B9C084d216F40963c070Eef95033B";
--- migrations/8_nft_staking_migration.js
+++ migrations/8_nft_staking_migration.js
@@ -17,7 +17,7 @@ module.exports = async function(deployer, network) {
 	
         await deployer.deploy(NftStaking, "0x168840Df293413A930d3D40baB6e1Cd8F406719D", "0xF06CF016b6DAdED5f676EE6340fc7398CA2142b0", "0x7115ABcCa5f0702E177f172C1c14b3F686d6A63a",
 			{gas: gasValue, gasPrice: gasPrice}).then(function(){
-	    	console.log("Nft Staking contract was deployed at address: "+NftStaking.address);
+	    	console.log("Staking Saloon contract was deployed at address: "+NftStaking.address);
 		});
     } else {
 		let crowns = "0x4Ca0ACab9f6B9C084d216F40963c070Eef95033B";
