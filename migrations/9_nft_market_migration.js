9bf653be403218e1b74bb0dec7a8fd266413c091
SchneiderNejc <nejc.sch@gmail.com>
2021-04-16 19:58:18 +0200

    update migration file

--- migrations/9_nft_market_migration.js
+++ migrations/9_nft_market_migration.js
@@ -5,22 +5,24 @@ var Nft = artifacts.require("./SeascapeNft.sol");
 
 
 
-// module.exports = function(deployer, network) {
-// 	deployer.then(async () => {
-//
-// 	await deployer.deploy(Crowns);
-// 	console.log("It is used with Crowns (CWS) Token at address: "+Crowns.address);
-// 	await deployer.deploy(NftMarket, Crowns.address);
-// 	console.log("NftMarket contract was deployed at address: "+NftMarket.address);
-// 	});
-// };
-
-
 module.exports = function(deployer, network) {
-	deployer.deploy(NftMarket, Crowns.address, Nft.address).then(function(){
-	    console.log("Staking contract was deployed at address: "+NftMarket.address);
-	    console.log("It is using Crowns (CWS) Token at address: "+Crowns.address);
-	    console.log("To mint Nft it is using NFT Factory at address: "+Nft.address);
+	deployer.then(async () => {
 
+	await deployer.deploy(Nft);
+	console.log("To mint Nft it is using NFT Factory at address: "+Nft.address);
+	await deployer.deploy(Crowns);
+	console.log("It is used with Crowns (CWS) Token at address: "+Crowns.address);
+	await deployer.deploy(NftMarket, Crowns.address, Nft.address);
+	console.log("NftMarket contract was deployed at address: "+NftMarket.address);
 	});
 };
+
+
+// module.exports = function(deployer, network) {
+// 	deployer.deploy(NftMarket, Crowns.address, Nft.address).then(function(){
+// 	    console.log("Staking contract was deployed at address: "+NftMarket.address);
+// 	    console.log("It is using Crowns (CWS) Token at address: "+Crowns.address);
+// 	    console.log("To mint Nft it is using NFT Factory at address: "+Nft.address);
+//
+// 	});
+// };
