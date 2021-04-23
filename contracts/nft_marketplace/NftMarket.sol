19d5ff55c23c43f01ed5398b218935e45a0e576f
SchneiderNejc <nejc.sch@gmail.com>
2021-04-23 12:14:55 +0200

    add comments

--- contracts/nft_marketplace/NftMarket.sol
+++ contracts/nft_marketplace/NftMarket.sol
@@ -15,42 +15,41 @@ import "./ReentrancyGuard.sol";
 
 
 
-
-
 contract NftMarket is IERC721Receiver,  ReentrancyGuard, Ownable {
 
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
-    // native token
+    // seascape token
     IERC20 public crowns;
     SeascapeNft private nft;
 
     // --- Data ---
-    bool private initialized; // Flag of initialize data
+    bool private initialized; // Flag of data initialized
 
 
     struct SalesObject {
         uint256 id;
         uint256 tokenId;
-        uint256 startTime;
-        uint256 maxPrice;
-        uint256 finalPrice;
-        uint8 status;
-        address payable seller;
-        address payable buyer;
+        uint256 startTime;        // timestamp when the sale starts
+        uint256 maxPrice;         // remove, the same as finalPrice
+        uint256 finalPrice;       // nf price
+        uint8 status;             // 2 = sale canceled, 1 = sold, 0 = for sale
+        address payable seller;   // seller address
+        address payable buyer;    // buyer address
     }
 
-    uint256 public _salesAmount;
 
-    SalesObject[] _salesObjects;
+    uint256 public _salesAmount;   // keeps count of sales
+
+    SalesObject[] _salesObjects;  // store sales in an array
 
     mapping(address => bool) public _supportNft;
-    bool public _isStartUserSales;
+    bool public _isStartUserSales;  // enable/disable trading
 
-    uint256 public _tipsFeeRate = 20;
+    uint256 public _tipsFeeRate = 20; // feeAmount = (_tipsFeeRate / _baseRate) * price
     uint256 public _baseRate = 1000;
-    address payable _tipsFeeWallet;
+    address payable _tipsFeeWallet; // reciever of fees
 
     event Buy(
         uint256 indexed id,
@@ -82,18 +81,17 @@ contract NftMarket is IERC721Receiver,  ReentrancyGuard, Ownable {
     mapping(uint256=>address) public routers;
 
 
-
-
     constructor(address _crowns, address _nft) public {
       crowns = IERC20(_crowns);
       nft = SeascapeNft(_nft);
     }
 
+    // recieve tokens
     receive() external payable { }
     //@note Prefer using a receive function only, whenever possible
     //fallback() external [payable] { }
 
-    // --- Init ---
+    // set tips data - may be moved to constructor
     function initialize(
         address payable tipsFeeWallet,
         uint256 tipsFeeRate,
@@ -116,12 +114,13 @@ contract NftMarket is IERC721Receiver,  ReentrancyGuard, Ownable {
         _;
     }
 
+    // index cant be higher than sales amount
     modifier checkindex(uint index) {
         require(index <= _salesObjects.length, "overflow");
         _;
     }
 
-    //only used in buy
+
     modifier checkTime(uint index) {
         require(index <= _salesObjects.length, "overflow");
         SalesObject storage obj = _salesObjects[index];
@@ -145,149 +144,155 @@ contract NftMarket is IERC721Receiver,  ReentrancyGuard, Ownable {
         _;
     }
 
-
+  // enable/disable trading
   function setIsStartUserSales(bool isStartUserSales) public onlyOwner {
       _isStartUserSales = isStartUserSales;
   }
 
+  // set address to recieve fees
   function setTipsFeeWallet(address payable wallet) public onlyOwner {
       _tipsFeeWallet = wallet;
   }
 
+  // adjust base rate - can be deleted
   function setBaseRate(uint256 rate) external onlyOwner {
       _baseRate = rate;
   }
 
+  // adjust tips rate in percents
   function setTipsFeeRate(uint256 rate) external onlyOwner {
       _tipsFeeRate = rate;
   }
 
-function getSales(uint index) external view checkindex(index) returns(SalesObject memory) {
-    return _salesObjects[index];
-}
+  // returns sales by index
+  function getSales(uint index) external view checkindex(index) returns(SalesObject memory) {
+      return _salesObjects[index];
+  }
 
-function getSalesPrice(uint index)
-    external
-    view
-    checkindex(index)
-    returns (uint256)
-{
+  // returns just the price of sale by index - may be deleted
+  function getSalesPrice(uint index)
+      external
+      view
+      checkindex(index)
+      returns (uint256)
+  {
+      SalesObject storage obj = _salesObjects[index];
+      return obj.maxPrice;
+  }
+
+  // cancel a sale - only nft owner can call
+  function cancelSales(uint index) external checkindex(index) onlySalesOwner(index) mustNotSellingOut(index) nonReentrant {
+      require(_isStartUserSales, "cannot sales");
+      SalesObject storage obj = _salesObjects[index];
+      obj.status = 2;
+      nft.safeTransferFrom(address(this), obj.seller, obj.tokenId);
+
+      emit SaleCanceled(index, obj.tokenId);
+  }
+
+  // put nft for sale
+  function startSales(uint256 tokenId,
+                      uint256 maxPrice,
+                      uint256 startTime,
+                      address currency)
+      external
+      nonReentrant
+      returns(uint)
+  {
+      require(tokenId != 0, "invalid token");
+      require(startTime > now, "sale must start in the future");
+      require(_isStartUserSales, "cannot sales");
+
+      nft.safeTransferFrom(msg.sender, address(this), tokenId);
+
+      _salesAmount++;
+      SalesObject memory obj;
+
+      obj.id = _salesAmount;
+      obj.tokenId = tokenId;
+      obj.seller = msg.sender;
+      obj.buyer = address(0x0);
+      obj.startTime = startTime;
+      obj.maxPrice = maxPrice;
+      obj.finalPrice = 0;
+      obj.status = 0;
+
+      _saleOnCurrency[obj.id] = currency;
+
+      if (_salesObjects.length == 0) {
+          SalesObject memory zeroObj;
+          zeroObj.tokenId = 0;
+          zeroObj.seller = address(0x0);
+          zeroObj.buyer = address(0x0);
+          zeroObj.startTime = 0;
+          zeroObj.maxPrice = 0;
+          zeroObj.finalPrice = 0;
+          zeroObj.status = 2;
+          _salesObjects.push(zeroObj);
+      }
+
+      _salesObjects.push(obj);
+
+      uint256 tmpMaxPrice = maxPrice;
+      emit Sell(obj.id, tokenId, msg.sender, address(0x0), startTime, tmpMaxPrice, 0);
+      return _salesAmount;
+  }
+
+  // upon transfer, log nft reciept
+  function onERC721Received(address operator, address from, uint256 tokenId, bytes memory data) public override returns (bytes4) {
+     //only receive the _nft staff
+     if(address(this) != operator) {
+         //invalid from nft
+         return 0;
+     }
+
+     //success
+     emit eveNFTReceived(operator, from, tokenId, data);
+     return bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"));
+  }
+
+  // buy nft
+  function buy(uint index, address currency_)
+      public
+      nonReentrant
+      mustNotSellingOut(index)
+      checkTime(index)
+      payable
+  {
     SalesObject storage obj = _salesObjects[index];
-    return obj.maxPrice;
-}
-
-function cancelSales(uint index) external checkindex(index) onlySalesOwner(index) mustNotSellingOut(index) nonReentrant {
-    require(_isStartUserSales, "cannot sales");
-    SalesObject storage obj = _salesObjects[index];
-    obj.status = 2;
-    nft.safeTransferFrom(address(this), obj.seller, obj.tokenId);
-
-    emit eveCancelSales(index, obj.tokenId);
-}
-
-function startSales(uint256 tokenId,
-                    uint256 maxPrice,
-                    uint256 startTime,
-                    address currency)
-    external
-    nonReentrant
-    returns(uint)
-{
-    require(tokenId != 0, "invalid token");
-    require(startTime > now, "invalid start time");
     require(_isStartUserSales, "cannot sales");
 
-    nft.safeTransferFrom(msg.sender, address(this), tokenId);
+    address currencyAddr = _saleOnCurrency[obj.id];
+    uint256 price = this.getSalesPrice(index);
+    uint256 tipsFee = price.mul(_tipsFeeRate).div(_baseRate);
+    uint256 purchase = price.sub(tipsFee);
 
-    _salesAmount++;
-    SalesObject memory obj;
-
-    obj.id = _salesAmount;
-    obj.tokenId = tokenId;
-    obj.seller = msg.sender;
-    obj.buyer = address(0x0);
-    obj.startTime = startTime;
-    obj.maxPrice = maxPrice;
-    obj.finalPrice = 0;
-    obj.status = 0;
-
-    _saleOnCurrency[obj.id] = currency;
-
-    if (_salesObjects.length == 0) {
-        SalesObject memory zeroObj;
-        zeroObj.tokenId = 0;
-        zeroObj.seller = address(0x0);
-        zeroObj.buyer = address(0x0);
-        zeroObj.startTime = 0;
-        zeroObj.maxPrice = 0;
-        zeroObj.finalPrice = 0;
-        zeroObj.status = 2;
-        _salesObjects.push(zeroObj);
+    require(address(currencyAddr) == currency_, "must use same currency as seller");
+    if (currencyAddr == address(0x0)){
+        require (msg.value >= this.getSalesPrice(index), "umm.....  your price is too low");
+        uint256 returnBack = msg.value.sub(price);
+        if(returnBack > 0) {
+            msg.sender.transfer(returnBack);
+        }
+        if(tipsFee > 0) {
+            _tipsFeeWallet.transfer(tipsFee);
+        }
+        obj.seller.transfer(purchase);
+    }
+    else{
+        IERC20(currencyAddr).safeTransferFrom(msg.sender, _tipsFeeWallet, tipsFee);
+        IERC20(currencyAddr).safeTransferFrom(msg.sender, obj.seller, purchase);
     }
 
-    _salesObjects.push(obj);
+      nft.safeTransferFrom(address(this), msg.sender, obj.tokenId);
 
+      obj.buyer = msg.sender;
+      obj.finalPrice = price;
 
-    uint256 tmpMaxPrice = maxPrice;
-    emit Sell(obj.id, tokenId, msg.sender, address(0x0), startTime, tmpMaxPrice, 0);
-    return _salesAmount;
-}
+      obj.status = 1;
 
-
-function onERC721Received(address operator, address from, uint256 tokenId, bytes memory data) public override returns (bytes4) {
-   //only receive the _nft staff
-   if(address(this) != operator) {
-       //invalid from nft
-       return 0;
-   }
-
-   //success
-   emit eveNFTReceived(operator, from, tokenId, data);
-   return bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"));
-}
-
-
-function buy(uint index, address currency_)
-    public
-    nonReentrant
-    mustNotSellingOut(index)
-    checkTime(index)
-    payable
-{
-  SalesObject storage obj = _salesObjects[index];
-  require(_isStartUserSales, "cannot sales");
-
-  address currencyAddr = _saleOnCurrency[obj.id];
-  uint256 price = this.getSalesPrice(index);
-  uint256 tipsFee = price.mul(_tipsFeeRate).div(_baseRate);
-  uint256 purchase = price.sub(tipsFee);
-
-  require(address(currencyAddr) == currency_, "must use same currency as seller");
-  if (currencyAddr == address(0x0)){
-      require (msg.value >= this.getSalesPrice(index), "umm.....  your price is too low");
-      uint256 returnBack = msg.value.sub(price);
-      if(returnBack > 0) {
-          msg.sender.transfer(returnBack);
-      }
-      if(tipsFee > 0) {
-          _tipsFeeWallet.transfer(tipsFee);
-      }
-      obj.seller.transfer(purchase);
+      // fire event
+      emit Buy(index, obj.tokenId, msg.sender, price, tipsFee);
   }
-  else{
-      IERC20(currencyAddr).safeTransferFrom(msg.sender, _tipsFeeWallet, tipsFee);
-      IERC20(currencyAddr).safeTransferFrom(msg.sender, obj.seller, purchase);
-  }
-
-    nft.safeTransferFrom(address(this), msg.sender, obj.tokenId);
-
-    obj.buyer = msg.sender;
-    obj.finalPrice = price;
-
-    obj.status = 1;
-
-    // fire event
-    emit Buy(index, obj.tokenId, msg.sender, price, tipsFee);
-}
 
 }
