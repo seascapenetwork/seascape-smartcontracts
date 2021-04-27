b0fd26fb198a267cf2beff4f1435efa71c31bdb9
SchneiderNejc <nejc.sch@gmail.com>
2021-04-27 13:15:40 +0200

    migrate based on selected network

--- migrations/9_nft_market_migration.js
+++ migrations/9_nft_market_migration.js
@@ -9,15 +9,23 @@ async function getAccount(id) {
     return accounts[id];
 }
 
-
-var feesReciever = getAccount(3);
 const tipsFeeRate = 100;	// 10%
 
 module.exports = function(deployer, network) {
-	deployer.deploy(NftMarket, Crowns.address, Nft.address, feesReciever, tipsFeeRate)
-	 .then(function(){
-	    console.log("Market contract was deployed at address: "+NftMarket.address);
-	    console.log("It is using Crowns (CWS) Token at address: "+Crowns.address);
-	    console.log("To mint Nft it is using NFT Factory at address: "+Nft.address);
-	});
+      if (network == "ganache") {
+        var feesReciever = getAccount(3);
+        deployer.deploy(NftMarket, Crowns.address, Nft.address, feesReciever, tipsFeeRate)
+         .then(function(){
+            console.log("Market contract was deployed at address: "+NftMarket.address);
+            console.log("It is using Crowns (CWS) Token at address: "+Crowns.address);
+            console.log("To mint Nft it is using NFT Factory at address: "+Nft.address);
+        });
+        } else if (network == "rinkeby") {
+          var feesReciever = "0x5bDed8f6BdAE766C361EDaE25c5DC966BCaF8f43";
+
+          deployer.deploy(NftMarket, "0x168840Df293413A930d3D40baB6e1Cd8F406719D", "0x7115ABcCa5f0702E177f172C1c14b3F686d6A63a", feesReciever, tipsFeeRate)
+           .then(function(){
+              console.log("Market contract was deployed at address: "+NftMarket.address);
+          });
+        }
 };
