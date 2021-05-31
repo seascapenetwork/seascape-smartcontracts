e8d48bf3381894b8607727a40e31e19cd2952491
ahmetson <admin@blocklords.io>
2021-05-31 11:27:25 +0700

    Add ganache for docker

---
+++ ganache/ganache_with_accounts.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# I don't have a better way to pass double quotes to docker CMD/ENTRYPOINT
+# Sorry about it :P
+node /app/ganache-core.docker.cli.js --quiet --account="$ACCOUNT_1,100000000000000000000" --account="$ACCOUNT_2,100000000000000000000" --account="$ACCOUNT_3,100000000000000000000" -b 1 -i $NETWORK_ID
