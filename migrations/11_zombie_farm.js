376b88acd562dcf51e7d9b3c7c180c6481301492
ahmetson <admin@blocklords.io>
2021-07-29 11:15:58 +0700

    Add single token challenge category to migration

--- migrations/11_zombie_farm.js
+++ migrations/11_zombie_farm.js
@@ -1,5 +1,6 @@
 var ZombieFarm = artifacts.require("./ZombieFarm.sol");
 var ScapeNftReward = artifacts.require("./ScapeNftReward.sol");
+var SingleTokenChallenge = artifacts.require("./SingleTokenChallenge.sol");
 var Factory = artifacts.require("./NftFactory.sol");
 
 async function getAccount(id) {
@@ -13,7 +14,9 @@ module.exports = async function(deployer, network) {
 
         await deployer.deploy(ZombieFarm);
         await deployer.deploy(ScapeNftReward, Factory.address, ZombieFarm.address, pool);
+        await deployer.deploy(SingleTokenChallenge, ZombieFarm.address, pool);
 		console.log("ZombieFarm contract was deployed at address: " + ZombieFarm.address);
 		console.log("ScapeNftReward contract was deployed at address: " + ScapeNftReward.address);
+		console.log("SingleTokenChallenge contract was deployed at address: " + SingleTokenChallenge.address);
     }
 };
