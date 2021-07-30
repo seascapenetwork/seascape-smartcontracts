498c98d71734e5df39b9b66aa0fdf15314081ea6
ahmetson <admin@blocklords.io>
2021-07-30 16:06:02 +0700

    Add rinkeby migration of zombie farm

--- migrations/11_zombie_farm.js
+++ migrations/11_zombie_farm.js
@@ -18,5 +18,16 @@ module.exports = async function(deployer, network) {
 		console.log("ZombieFarm contract was deployed at address: " + ZombieFarm.address);
 		console.log("ScapeNftReward contract was deployed at address: " + ScapeNftReward.address);
 		console.log("SingleTokenChallenge contract was deployed at address: " + SingleTokenChallenge.address);
+    } else if (network == "rinkeby") {
+        let pool = await getAccount(0);
+        let factoryAddress = "0xF06CF016b6DAdED5f676EE6340fc7398CA2142b0";
+
+        await deployer.deploy(ZombieFarm);
+        await deployer.deploy(ScapeNftReward, factoryAddress, ZombieFarm.address, pool);
+        await deployer.deploy(SingleTokenChallenge, ZombieFarm.address, pool);
+
+        console.log("ZombieFarm contract was deployed at address: " + ZombieFarm.address);
+		console.log("ScapeNftReward contract was deployed at address: " + ScapeNftReward.address);
+		console.log("SingleTokenChallenge contract was deployed at address: " + SingleTokenChallenge.address);
     }
 };
