4ab07195060be138a382af520bef644bf42b9806
ahmetson <ahmetson@zoho.com>
2021-08-06 20:45:29 +0700

    add a farm migration

--- migrations/12_farm.js
+++ migrations/12_farm.js
@@ -1,5 +1,6 @@
 var FarmCake = artifacts.require("./FarmCake.sol");
 var SeascapeHodl = artifacts.require("./SeascapeHodl.sol");
+var FarmCake = artifacts.require("./FarmCake.sol");
 
 var ZombieFarm = artifacts.require("./ZombieFarm.sol");
 var ScapeNftReward = artifacts.require("./ScapeNftReward.sol");
@@ -8,6 +9,19 @@ var LpChallenge = artifacts.require("./LpChallenge.sol");
 var Factory = artifacts.require("./NftFactory.sol");
 var Crowns = artifacts.require("./CrownsToken.sol");
 
+/// The Game that can interact with hodler
+const testGame = "0x8A97Cc7e7407475C100eE9BE6712C4Ce7d39Cd26";
+/// Stake this token to farm another token. This is LP token of BNB-CWS
+const lpToken = "0x6615CE60D71513aA4849269dD63821D324A23F8C";
+/// ID of farming of Cake with BNB-CWS
+const pid = 333;
+/// The PancakeSwap main contract that farms
+const pancakeSwap = "0x73feaa1eE314F8c655E354234017bE2193C9E24E";
+/// The Account that will receive the Farmed Cakes
+const reward = "0x6342559B3C5Fc8E06Ff616936bDBc5FB1a471cBe";
+/// The token that is farmed
+const farm = "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82";
+
 async function getAccount(id) {
     let accounts = await web3.eth.getAccounts();
     return accounts[id];
@@ -15,31 +29,18 @@ async function getAccount(id) {
 
 module.exports = async function(deployer, network) {
     if (network == "development") {
-        let pool = await getAccount(0);
-
-        await deployer.deploy(ZombieFarm, Crowns.address);
-        await deployer.deploy(ScapeNftReward, Factory.address, ZombieFarm.address, pool);
-        await deployer.deploy(SingleTokenChallenge, ZombieFarm.address, pool);
-        await deployer.deploy(LpChallenge, ZombieFarm.address, pool);
+        await deployer.deploy(SeascapeHodl, testGame);
+        // constructor(address _stake, uint256 _pid, address _staking, address _hodl, address _reward, address _cake)
+        await deployer.deploy(FarmCake, lpToken, pid, pancakeSwap, SeascapeHodl.address, reward, farm);
         
-        console.log("ZombieFarm contract was deployed at address: " + ZombieFarm.address);
-		console.log("ScapeNftReward contract was deployed at address: " + ScapeNftReward.address);
-		console.log("SingleTokenChallenge contract was deployed at address: " + SingleTokenChallenge.address);
-		console.log("LpChallenge contract was deployed at address: " + LpChallenge.address);
-    } else if (network == "rinkeby") {
-        let pool = await getAccount(0);
-
-        let factoryAddress = "0xF06CF016b6DAdED5f676EE6340fc7398CA2142b0";
-        let crownsAddress = "0x168840Df293413A930d3D40baB6e1Cd8F406719D";
-
-        await deployer.deploy(ZombieFarm, crownsAddress);
-        await deployer.deploy(ScapeNftReward, factoryAddress, ZombieFarm.address, pool);
-        await deployer.deploy(SingleTokenChallenge, ZombieFarm.address, pool);
-        await deployer.deploy(LpChallenge, ZombieFarm.address, pool);
-
-        console.log("ZombieFarm contract was deployed at address: " + ZombieFarm.address);
-		console.log("ScapeNftReward contract was deployed at address: " + ScapeNftReward.address);
-		console.log("SingleTokenChallenge contract was deployed at address: " + SingleTokenChallenge.address);
-		console.log("LpChallenge contract was deployed at address: " + LpChallenge.address);
+        console.log("SeacapeHodl contract was deployed at address: " + SeascapeHodl.address);
+		console.log("FarmCake contract was deployed at address: " + FarmCake.address);
+    } else if (network == "bsc") {
+        await deployer.deploy(SeascapeHodl, testGame);
+        // constructor(address _stake, uint256 _pid, address _staking, address _hodl, address _reward, address _cake)
+        await deployer.deploy(FarmCake, lpToken, pid, pancakeSwap, SeascapeHodl.address, reward, farm);
+        
+        console.log("SeacapeHodl contract was deployed at address: " + SeascapeHodl.address);
+		console.log("FarmCake contract was deployed at address: " + FarmCake.address);
     }
 };
