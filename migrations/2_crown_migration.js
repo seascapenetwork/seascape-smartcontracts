7767ec7dbeec704fcda28ec3b62dd534bbe59e8f
ahmetson <ahmetson@zoho.com>
2021-08-06 23:23:17 +0700

    Crowns use test

--- migrations/2_crown_migration.js
+++ migrations/2_crown_migration.js
@@ -1,7 +1,10 @@
 var Crowns = artifacts.require("./CrownsToken.sol");
 
+let test = false;
+
 module.exports = function(deployer, network) {
-    deployer.deploy(Crowns).then(function(){
+
+    deployer.deploy(Crowns, test).then(function(){
 	    console.log("Crowns was deployed at: "+Crowns.address);
     });
 }
