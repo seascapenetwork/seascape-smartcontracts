3ea9056b867c0e4fd88ea9c37061f3ef3ed6903f
ahmetson <ahmetson@zoho.com>
2021-08-10 20:47:30 +0700

    Profit Circus season starting works with minimul staking

--- scripts/profit_circus/init.js
+++ scripts/profit_circus/init.js
@@ -9,6 +9,9 @@ let reward = web3.utils.toWei("20", "ether");  // cws
 let period = 3600 * 24 * 1;
 let generation = 1;
 
+let stakeAmount = web3.utils.toWei("1", "ether"); // 1 LP token in ETHER format.
+let stakePeriod = 3600;
+
 /**
  * For test purpose, starts a game session
  */
@@ -23,22 +26,22 @@ let init = async function(networkId) {
     accounts = await web3.eth.getAccounts();
     console.log(accounts)
 
-	let profitCircus    = await ProfitCircus.at("0xa35abb86c53695bb1b23b55808b6c5871432c22c");
+	let profitCircus    = await ProfitCircus.at("0xBaD4475CB470933c141A6613C9404d46564BF160");
 	let factory         = await Factory.at("0x25F4C38FAF75dF9622FECB17Fa830278cd732091");
 	let crowns          = await Crowns.at("0xac0104cca91d167873b8601d2e71eb3d4d8c33e0");	
     let lpTokenAddress  = "0xac0104cca91d167873b8601d2e71eb3d4d8c33e0";
 
     console.log("Set the contracts");
 
-    //await factory.addStaticUser(profitCircus.address).catch(console.error);
-    //console.log("Profit Circus contract got a permission to mint nfts");
+    await factory.addStaticUser(profitCircus.address).catch(console.error);
+    console.log("Profit Circus contract got a permission to mint nfts");
 
     // should transfer reward amount to contract
     await crowns.transfer(profitCircus.address, reward, {from: accounts[0]});
     console.log("Crowns for session were transferred to the Lp Mining smartcontract");
 
     let startTime = Math.floor(new Date().getTime()/1000) + 30;
-    await profitCircus.startSession(lpTokenAddress, reward, period, startTime, generation);
+    await profitCircus.startSession(lpTokenAddress, reward, period, startTime, generation, stakeAmount, stakePeriod);
     console.log("Session started");
 
     let sessionId = await profitCircus.lastSessionIds.call(lpTokenAddress);
