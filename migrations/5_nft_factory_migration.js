ae711d434ce6b11c872400433f73412eb863711f
SchneiderNejc <nejc.sch@gmail.com>
2021-08-25 01:49:25 +0200

    update migrations with moonbase config

--- migrations/5_nft_factory_migration.js
+++ migrations/5_nft_factory_migration.js
@@ -2,8 +2,7 @@ var Nft = artifacts.require("./SeascapeNft.sol");
 var Factory = artifacts.require("./NftFactory.sol");
 
 module.exports = function(deployer, network) {
-    deployer.deploy(Factory, Nft.address).then(function(){
+    deployer.deploy(Factory, "0xf0c27DB379AD997C225701EAf00344693B4b36e1").then(function(){
 	    console.log("Nft Factory was deployed at address: "+Factory.address);
     });
 };
- 
--- migrations/6_profit_circus_migration.js
+++ migrations/6_profit_circus_migration.js
@@ -6,25 +6,9 @@ module.exports = async function(deployer, network) {
 	let crowns;
 	let factory;
 
-    if (network == "development") {
-		crowns = Crowns.address;
-		factory = NftFactory.address;
-    } else if (network == "rinkeby") {
-		crowns = "0x168840df293413a930d3d40bab6e1cd8f406719d";
-		factory = "0xF06CF016b6DAdED5f676EE6340fc7398CA2142b0";
-	} else if (network == 'mainnet') {
-		crowns = "0xac0104cca91d167873b8601d2e71eb3d4d8c33e0";
-		factory = "0x25F4C38FAF75dF9622FECB17Fa830278cd732091";
-	} else if (network == 'bsc') {
-		crowns = "0xbcf39F0EDDa668C58371E519AF37CA705f2bFcbd";
-		factory = "0xa304D289f6d0a30aEB33e9243f47Efa3a9ad437d";
-    } else if (network == 'bsctestnet') {
-		// bsc-testnet
-		crowns = "0x4Ca0ACab9f6B9C084d216F40963c070Eef95033B";
-		factory = "0x3eB88c3F2A719369320D731FbaE062b0f82F22e4";
-	} else {
-		throw `${network} is not supported`;
-	}
+		crowns = "0xfC3C4136f8b2E19a6a759601D1aa4e29A8A502A1";
+		factory = "0x3fd6Db81DD05e6054CBcddB0b2D4De37db2886d9";
+
 
 	deployer.deploy(ProfitCircus, crowns, factory)
 	.then(() => {
