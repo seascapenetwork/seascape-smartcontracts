33827bfeffa5026202bbfc4f4b8eb0606c30c778
ahmetson <ahmetson@zoho.com>
2021-08-26 10:28:51 +0700

    Profit Circus: update the migration

--- migrations/6_profit_circus_migration.js
+++ migrations/6_profit_circus_migration.js
@@ -6,9 +6,8 @@ module.exports = async function(deployer, network) {
 	let crowns;
 	let factory;
 
-		crowns = "0xFde9cad69E98b3Cc8C998a8F2094293cb0bD6911";
-		factory = "0x06fddbD58cb286DC1e7a9eB50eF67c9215478670";
-
+	crowns = "0xFde9cad69E98b3Cc8C998a8F2094293cb0bD6911";
+	factory = "0x06fddbD58cb286DC1e7a9eB50eF67c9215478670";
 
 	deployer.deploy(ProfitCircus, crowns, factory)
 	.then(() => {
