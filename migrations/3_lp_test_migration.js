41be132deb33ef6194ffebd35618ba82bdd11a0a
ahmetson <ahmetson@zoho.com>
2021-08-27 15:51:53 +0700

    Change LP test token migration to Riverboat migration

--- migrations/3_lp_test_migration.js
+++
@@ -1,7 +0,0 @@
-var LpToken = artifacts.require("./LpToken.sol");
-
-module.exports = function(deployer, network) {
-		deployer.deploy(LpToken).then(function(){
-			console.log("Lp Test token contract was deployed at address: "+LpToken.address);
-		});
-};
