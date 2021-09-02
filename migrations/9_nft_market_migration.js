4d56b59a788012ecb8312ecc75b26711dd2fb979
ahmetson <ahmetson@zoho.com>
2021-09-03 01:25:30 +0700

    Scape Store: Add migration for Binance Smart Chain

--- migrations/9_nft_market_migration.js
+++ migrations/9_nft_market_migration.js
@@ -8,22 +8,31 @@ async function getAccount(id) {
 }
 
 
-const tipsFeeRate = 100;	// 1 = 0.1%, 100 = 10%
+const tipsFeeRate = 100;	// 1 = 0.1%, 10 = 1%, 100 = 10%
 
 module.exports = function(deployer, network) {
       if (network == "ganache") {
         var feesReciever = getAccount(3);
         deployer.deploy(NftMarket, feesReciever, tipsFeeRate)
-         .then(function(){
+          .then(function(){
             console.log("Market contract was deployed at address: "+NftMarket.address);
         });
       }
       else if (network == "rinkeby" || network == "moonbase") {
-          var feesReciever = "0x5bDed8f6BdAE766C361EDaE25c5DC966BCaF8f43";
+        var feesReciever = "0x5bDed8f6BdAE766C361EDaE25c5DC966BCaF8f43";
 
-          deployer.deploy(NftMarket, feesReciever, tipsFeeRate)
-           .then(function(){
-              console.log("Market contract was deployed at address: "+NftMarket.address);
+        deployer.deploy(NftMarket, feesReciever, tipsFeeRate)
+          .then(function(){
+            console.log("Market contract was deployed at address: "+NftMarket.address);
+        });
+      }
+      else if (network == 'bsc') {
+        let feesReciever = "0x155E13c0a337e80f5924732706Efe858D7003c20";
+
+        deployer.deploy(NftMarket, feesReciever, tipsFeeRate)
+          .then(function() {
+            console.log(`Scape Store on ${NftMarket.address}`);
+            console.log(`Now, set supported currencies by calling scripts/nft_market/init.js`);
           });
-        }
+      }
 };
