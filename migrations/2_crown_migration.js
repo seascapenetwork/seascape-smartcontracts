f2660dfbc95f4d91c13121613019934614f1c474
SchneiderNejc <nejc.sch@gmail.com>
2021-09-23 21:25:42 +0200

    add "type" variable to crowns_migration

--- migrations/2_crown_migration.js
+++ migrations/2_crown_migration.js
@@ -1,9 +1,9 @@
 var Crowns = artifacts.require("./CrownsToken.sol");
 
+let type = 1;
+
 module.exports = function(deployer, network) {
-    deployer.deploy(Crowns).then(function(){
+    deployer.deploy(Crowns, type).then(function(){
 	    console.log("Crowns was deployed at: "+Crowns.address);
     });
 }
-				 
-				 
