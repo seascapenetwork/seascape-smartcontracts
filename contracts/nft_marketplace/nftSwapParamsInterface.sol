ced6ab54b58bd54e72b73aec16cd098f7384d28a
SchneiderNejc <m2ns9nsk0M82str0l>
2021-10-13 15:17:29 +0200

    rename function isValidParams to metadataIsValid in NftMetadataInterface.sol

--- contracts/nft_marketplace/nftSwapParamsInterface.sol
+++ contracts/nft_marketplace/nftSwapParamsInterface.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.6.7;
 interface NftMetadataInterface {
 
     /// @dev Returns true if signature is valid
-    function isValidParams(uint256 _offerId, bytes calldata _encodedParams,
+    function metadataIsValid(uint256 _offerId, bytes calldata _encodedParams,
         uint8 v, bytes32 r, bytes32 s) external view returns (bool);
 
 }
