58e765946eb269aecaf1bbc449f495466c75df3b
SchneiderNejc <nejc@seascape.network>
2021-10-27 23:21:11 +0200

    fix migration file in Riverboat

--- migrations/12_riverboat_migration.js
+++ migrations/12_riverboat_migration.js
@@ -1,36 +1,33 @@
 var Riverboat = artifacts.require("./Riverboat.sol");
 var RiverboatNft = artifacts.require("./RiverboatNft.sol");
-var RiverboatFactory = artifacts.require("./NftRiverboatFactory.sol");
+var RiverboatFactory = artifacts.require("./RiverboatFactory.sol");
 
 
 
-const _priceReceiver = async function getAccount(id) {
+async function getAccount(id) {
     let accounts = await web3.eth.getAccounts();
     return accounts[id];
 }
-const _priceReceiver= getAccount(0)
-console.log(_priceReceiver)
-                        // fee = _feeRate * 1000
-                        // e.g. if _feeRate is set to 1000 than fee will be 1 CWS
+
 
 module.exports = async function(deployer, network) {
+    var priceReceiver = await getAccount(0);
+
     if (network == "ganache") {
-        deployer.deploy(Riverboat, _priceReceiver.then(function(){
+        await deployer.deploy(Riverboat, priceReceiver).then(function(){
             console.log("Riverboat contract was deployed at address: "+Riverboat.address);
-            console.log("It is using Crowns (CWS) Token at address: "+Crowns.address);
         });
-        deployer.deploy(RiverboatNft).then(function(){
+        await deployer.deploy(RiverboatNft).then(function(){
             console.log("RiverboatNft contract was deployed at address: "+RiverboatNft.address);
         });
-        deployer.deploy(RiverboatFactory, RiverboatNft.address).then(function(){
+        await deployer.deploy(RiverboatFactory, RiverboatNft.address).then(function(){
             console.log("RiverboatFactory contract was deployed at address: "+RiverboatFactory.address);
         });
 
     } else if (network == "rinkeby") {
         var _RiverboatNft = "0x168840Df293413A930d3D40baB6e1Cd8F406719D";
-        deployer.deploy(Riverboat, _priceReceiver.then(function(){
+        deployer.deploy(Riverboat, priceReceiver).then(function(){
             console.log("Riverboat contract was deployed at address: "+Riverboat.address);
-            console.log("It is using Crowns (CWS) Token at address: "+Crowns.address);
         });
         deployer.deploy(RiverboatNft).then(function(){
             console.log("RiverboatNft contract was deployed at address: "+RiverboatNft.address);
@@ -41,9 +38,8 @@ module.exports = async function(deployer, network) {
 
     } else if (network == "bsctestnet") {
         var _RiverboatNft = "0x168840Df293413A930d3D40baB6e1Cd8F406719D";
-        deployer.deploy(Riverboat, _priceReceiver.then(function(){
+        deployer.deploy(Riverboat, priceReceiver).then(function(){
             console.log("Riverboat contract was deployed at address: "+Riverboat.address);
-            console.log("It is using Crowns (CWS) Token at address: "+Crowns.address);
         });
         deployer.deploy(RiverboatNft).then(function(){
             console.log("RiverboatNft contract was deployed at address: "+RiverboatNft.address);
@@ -51,13 +47,11 @@ module.exports = async function(deployer, network) {
         deployer.deploy(RiverboatFactory, _RiverboatNft).then(function(){
             console.log("RiverboatFactory contract was deployed at address: "+RiverboatFactory.address);
         });
-        
+
     } else if (network == "moonbase") {
         var _RiverboatNft = "0x168840Df293413A930d3D40baB6e1Cd8F406719D";
-
-        deployer.deploy(Riverboat, _priceReceiver.then(function(){
+        deployer.deploy(Riverboat, priceReceiver).then(function(){
             console.log("Riverboat contract was deployed at address: "+Riverboat.address);
-            console.log("It is using Crowns (CWS) Token at address: "+Crowns.address);
         });
         deployer.deploy(RiverboatNft).then(function(){
             console.log("RiverboatNft contract was deployed at address: "+RiverboatNft.address);
@@ -68,9 +62,8 @@ module.exports = async function(deployer, network) {
 
     } else if (network == "mainnet") {
         var _RiverboatNft = "0x168840Df293413A930d3D40baB6e1Cd8F406719D";
-        deployer.deploy(Riverboat, _priceReceiver.then(function(){
+        deployer.deploy(Riverboat, priceReceiver).then(function(){
             console.log("Riverboat contract was deployed at address: "+Riverboat.address);
-            console.log("It is using Crowns (CWS) Token at address: "+Crowns.address);
         });
         deployer.deploy(RiverboatNft).then(function(){
             console.log("RiverboatNft contract was deployed at address: "+RiverboatNft.address);
@@ -81,9 +74,8 @@ module.exports = async function(deployer, network) {
 
     } else if (network == "bsc") {
         var _RiverboatNft = "0x168840Df293413A930d3D40baB6e1Cd8F406719D";
-        deployer.deploy(Riverboat, _priceReceiver.then(function(){
+        deployer.deploy(Riverboat, priceReceiver).then(function(){
             console.log("Riverboat contract was deployed at address: "+Riverboat.address);
-            console.log("It is using Crowns (CWS) Token at address: "+Crowns.address);
         });
         deployer.deploy(RiverboatNft).then(function(){
             console.log("RiverboatNft contract was deployed at address: "+RiverboatNft.address);
