b609275cf1decc7a42c9098766fa218754df0280
SchneiderNejc <nejc@seascape.network>
2021-11-03 22:51:40 +0100

    add MscpToken.sol in mscp

---
+++ contracts/mscp/MscpToken.sol
@@ -0,0 +1,388 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.7;
+
+import "./../openzeppelin/contracts/access/Ownable.sol";
+import "./../openzeppelin/contracts/GSN/Context.sol";
+import "./../openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./../openzeppelin/contracts/math/SafeMath.sol";
+import "./../openzeppelin/contracts/utils/Address.sol";
+
+/// @title Official token of Moonscape ecosystem.
+/// @author Nejc Schneider
+/// @notice Use Moonscape in the game to obtain the premium resource MoonDust.
+/// @dev Implementation of the {IERC20} interface.
+contract MscpToken is Context, IERC20, Ownable {
+    using SafeMath for uint256;
+    using Address for address;
+
+    mapping (address => uint256) private _balances;
+    mapping (address => mapping (address => uint256)) private _allowances;
+
+    uint256 private _totalSupply;
+
+    string private constant _name = "Moonscape";
+    string private constant _symbol = "MSCP";
+    uint8 private immutable _decimals = 18;
+
+    uint256 private constant SCALER = 10 ** 18;
+
+    /// @notice Maximum possible supply of this token.
+    uint256 public limitSupply = 1111111111; /// 1.1 billion
+
+    /// @notice Set to false to stop mint/burn of token. Set to true to allow minting.
+    bool public bridgeAllowed = false;
+
+    /// @notice the list of bridge addresses allowed to mint tokens.
+    mapping(address => bool) public bridges;
+
+    // Mint and Burn
+    modifier onlyBridge {
+        require(bridgeAllowed && bridges[msg.sender]);
+        _;
+    }
+
+    event AddBridge(address indexed bridge);
+    event RemoveBridge(address indexed bridge);
+
+    /**
+     * @dev Sets the {name} and {symbol} of token.
+     * Initializes {decimals} with a default value of 18.
+     * Mints all tokens.
+     * Transfers ownership to another account. So, the token creator will not be counted as an owner.
+     */
+    constructor () public {
+        _mint(msg.sender, limitSupply);
+    }
+
+   function addBridge(address _bridge) external onlyOwner returns(bool) {
+        require(_bridge != address(0), "invalid address");
+        require(!bridges[_bridge], "bridge already added");
+
+        bridges[_bridge] = true;
+
+        emit AddBridge(_bridge);
+   }
+
+    function removeBridge(address _bridge) external onlyOwner returns(bool) {
+       require(_bridge != address(0), "invalid address");
+       require(bridges[_bridge], "bridge already removed");
+
+       bridges[_bridge] = false;
+
+       emit RemoveBridge(_bridge);
+   }
+
+
+   /**
+     * @dev Creates `amount` new tokens for `to`.
+     *
+     * See {ERC20-_mint}.
+     *
+     * Requirements:
+     *
+     * - the caller must have the `MINTER_ROLE`.
+     */
+    function mint(address to, uint256 amount) external onlyBridge {
+        require(_totalSupply.add(amount) <= limitSupply, "exceeded mint limit");
+        _mint(to, amount);
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from the caller.
+     *
+     * See {ERC20-_burn}.
+     *
+     * Included just to follow the standard of OpenZeppelin.
+     */
+    function burn(uint256 amount) public onlyBridge {
+        require(false, "Only burnFrom is allowed");
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from `account`, deducting from the caller's
+     * allowance.
+     *
+     * See {ERC20-_burn} and {ERC20-allowance}.
+     *
+     * Requirements:
+     *
+     * - the caller must have allowance for ``accounts``'s tokens of at least
+     * `amount`.
+     */
+    function burnFrom(address account, uint256 amount) public onlyBridge {
+        uint256 currentAllowance = allowance(account, _msgSender());
+        require(currentAllowance >= amount, "burn amount exceeds allowance");
+
+        _approve(account, _msgSender(), currentAllowance
+            .sub(amount, "transfer amount exceeds allowance"));
+        _burn(account, amount);
+    }
+
+    function toggleBridgeAllowance() external onlyOwner {
+        bridgeAllowed = !bridgeAllowed;
+    }
+
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() public pure returns (string memory) {
+        return _name;
+    }
+
+    /**
+     * @dev Returns the symbol of the token, usually a shorter version of the
+     * name.
+     */
+    function symbol() public pure returns (string memory) {
+        return _symbol;
+    }
+
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5,05` (`505 / 10 ** 2`).
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract, including
+     * {IERC20-balanceOf} and {IERC20-transfer}.
+     */
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
+
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() public view override returns (uint256) {
+        return _totalSupply;
+    }
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) public view override returns (uint256) {
+        return _getBalance(account);
+    }
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) public virtual override returns (bool) {
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount)
+        public
+        virtual
+        override
+        returns (bool)
+    {
+        _transfer(sender, recipient, amount);
+        _approve(sender, _msgSender(), _allowances[sender][_msgSender()]
+            .sub(amount, "transfer amount exceeds allowance"));
+        return true;
+    }
+
+    /**
+     * @dev Atomically increases the allowance granted to `spender` by the caller.
+     *
+     * This is an alternative to {approve} that can be used as a mitigation for
+     * problems described in {IERC20-approve}.
+     *
+     * Emits an {Approval} event indicating the updated allowance.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     */
+    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].add(addedValue));
+        return true;
+    }
+
+    /**
+     * @dev Atomically decreases the allowance granted to `spender` by the caller.
+     *
+     * This is an alternative to {approve} that can be used as a mitigation for
+     * problems described in {IERC20-approve}.
+     *
+     * Emits an {Approval} event indicating the updated allowance.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     * - `spender` must have allowance for the caller of at least
+     * `subtractedValue`.
+     */
+    function decreaseAllowance(address spender, uint256 subtractedValue)
+        public
+        virtual
+        returns (bool)
+    {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender]
+            .sub(subtractedValue, "cant decrease allowance below zero"));
+        return true;
+    }
+
+    /**
+     * @dev Moves tokens `amount` from `sender` to `recipient`.
+     *
+     * This is internal function is equivalent to {transfer}, and can be used to
+     * e.g. implement automatic token fees, slashing mechanisms, etc.
+     *
+     * Emits a {Transfer} event.
+     *
+     * Requirements:
+     *
+     * - `sender` cannot be the zero address.
+     * - `recipient` cannot be the zero address.
+     * - `sender` must have a balance of at least `amount`.
+     */
+    function _transfer(address sender, address recipient, uint256 amount) internal virtual {
+        require(sender != address(0), "cant transfer from zero address");
+        require(recipient != address(0), "cant transfer to zero address");
+        require(amount > 0, "Cant send 0 token");
+        require(_getBalance(sender) >= amount, "not enough token to send");
+
+        _beforeTokenTransfer(sender, recipient, amount);
+
+        _balances[sender] =  _balances[sender].sub(amount);
+        _balances[recipient] = _balances[recipient].add(amount);
+
+        emit Transfer(sender, recipient, amount);
+    }
+
+    /** @dev Creates `amount` tokens and assigns them to `account`, increasing
+     * the total supply.
+     *
+     * Emits a {Transfer} event with `from` set to the zero address.
+     *
+     * Requirements
+     *
+     * - `to` cannot be the zero address.
+     */
+    function _mint(address account, uint256 amount) internal virtual {
+        require(account != address(0), "cant mint to invalid address");
+
+        _beforeTokenTransfer(address(0), account, amount);
+
+        _totalSupply = _totalSupply.add(amount);
+        _balances[account] = _balances[account].add(amount);
+        emit Transfer(address(0), account, amount);
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from `account`, reducing the
+     * total supply.
+     *
+     * Emits a {Transfer} event with `to` set to the zero address.
+     *
+     * Requirements:
+     *
+     * - `account` cannot be the zero address.
+     * - `account` must have at least `amount` tokens.
+     */
+    function _burn(address account, uint256 amount) internal virtual {
+        require(account != address(0), "cant burn from the zero address");
+
+        _beforeTokenTransfer(account, address(0), amount);
+
+        uint256 accountBalance = _balances[account];
+        require(accountBalance >= amount, "burn amount exceeds balance");
+        _balances[account] = accountBalance.sub(amount);
+        _totalSupply = _totalSupply.sub(amount);
+
+        emit Transfer(account, address(0), amount);
+    }
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the `owner`s tokens.
+     *
+     * This is internal function is equivalent to `approve`, and can be used to
+     * e.g. set automatic allowances for certain subsystems, etc.
+     *
+     * Emits an {Approval} event.
+     *
+     * Requirements:
+     *
+     * - `owner` cannot be the zero address.
+     * - `spender` cannot be the zero address.
+     */
+    function _approve(address owner, address spender, uint256 amount) internal virtual {
+        require(owner != address(0), "cant approve from zero address");
+        require(spender != address(0), "cant approve to zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    /**
+     * @dev Hook that is called before any transfer of tokens. This includes
+     * minting and burning.
+     *
+     * Calling conditions:
+     *
+     * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
+     * will be to transferred to `to`.
+     * - when `from` is zero, `amount` tokens will be minted for `to`.
+     * - when `to` is zero, `amount` of ``from``'s tokens will be burned.
+     * - `from` and `to` are never both zero.
+     *
+     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
+     */
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual { }
+
+    /**
+     * @dev Returns actual balance of account as a sum of owned divends and current balance.
+     * @param account Address of Token holder.
+     * @return Token amount
+     */
+    function _getBalance(address account) private view returns (uint256) {
+        return _balances[account];
+    }
+
+}
