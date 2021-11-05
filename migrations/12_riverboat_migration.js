f47604ae3809ed47f16e0f54636c4f0ee3273701
SchneiderNejc <nejc@seascape.network>
2021-11-05 15:04:31 +0100

    update 12_riverboat_migration.js with rinkeby address in Riverboat

--- migrations/12_riverboat_migration.js
+++ migrations/12_riverboat_migration.js
@@ -25,62 +25,62 @@ module.exports = async function(deployer, network) {
         });
 
     } else if (network == "rinkeby") {
-        var _RiverboatNft = "0x168840Df293413A930d3D40baB6e1Cd8F406719D";
-        deployer.deploy(Riverboat, priceReceiver).then(function(){
-            console.log("Riverboat contract was deployed at address: "+Riverboat.address);
-        });
-        deployer.deploy(RiverboatNft).then(function(){
-            console.log("RiverboatNft contract was deployed at address: "+RiverboatNft.address);
-        });
-        deployer.deploy(RiverboatFactory, _RiverboatNft).then(function(){
+        var _RiverboatNft = "0x115Aa9E35564307365Ca3f215f67eB69886f2fD1";
+        // await deployer.deploy(Riverboat, priceReceiver).then(function(){
+        //     console.log("Riverboat contract was deployed at address: "+Riverboat.address);
+        // });
+        // await deployer.deploy(RiverboatNft).then(function(){
+        //     console.log("RiverboatNft contract was deployed at address: "+RiverboatNft.address);
+        // });
+        await deployer.deploy(RiverboatFactory, _RiverboatNft).then(function(){
             console.log("RiverboatFactory contract was deployed at address: "+RiverboatFactory.address);
         });
 
     } else if (network == "bsctestnet") {
         var _RiverboatNft = "0x168840Df293413A930d3D40baB6e1Cd8F406719D";
-        deployer.deploy(Riverboat, priceReceiver).then(function(){
+        await deployer.deploy(Riverboat, priceReceiver).then(function(){
             console.log("Riverboat contract was deployed at address: "+Riverboat.address);
         });
-        deployer.deploy(RiverboatNft).then(function(){
+        await deployer.deploy(RiverboatNft).then(function(){
             console.log("RiverboatNft contract was deployed at address: "+RiverboatNft.address);
         });
-        deployer.deploy(RiverboatFactory, _RiverboatNft).then(function(){
+        await deployer.deploy(RiverboatFactory, _RiverboatNft).then(function(){
             console.log("RiverboatFactory contract was deployed at address: "+RiverboatFactory.address);
         });
 
     } else if (network == "moonbase") {
         var _RiverboatNft = "0x168840Df293413A930d3D40baB6e1Cd8F406719D";
-        deployer.deploy(Riverboat, priceReceiver).then(function(){
+        await deployer.deploy(Riverboat, priceReceiver).then(function(){
             console.log("Riverboat contract was deployed at address: "+Riverboat.address);
         });
-        deployer.deploy(RiverboatNft).then(function(){
+        await deployer.deploy(RiverboatNft).then(function(){
             console.log("RiverboatNft contract was deployed at address: "+RiverboatNft.address);
         });
-        deployer.deploy(RiverboatFactory, _RiverboatNft).then(function(){
+        await deployer.deploy(RiverboatFactory, _RiverboatNft).then(function(){
             console.log("RiverboatFactory contract was deployed at address: "+RiverboatFactory.address);
         });
 
     } else if (network == "mainnet") {
         var _RiverboatNft = "0x168840Df293413A930d3D40baB6e1Cd8F406719D";
-        deployer.deploy(Riverboat, priceReceiver).then(function(){
+        await deployer.deploy(Riverboat, priceReceiver).then(function(){
             console.log("Riverboat contract was deployed at address: "+Riverboat.address);
         });
-        deployer.deploy(RiverboatNft).then(function(){
+        await deployer.deploy(RiverboatNft).then(function(){
             console.log("RiverboatNft contract was deployed at address: "+RiverboatNft.address);
         });
-        deployer.deploy(RiverboatFactory, _RiverboatNft).then(function(){
+        await deployer.deploy(RiverboatFactory, _RiverboatNft).then(function(){
             console.log("RiverboatFactory contract was deployed at address: "+RiverboatFactory.address);
         });
 
     } else if (network == "bsc") {
         var _RiverboatNft = "0x168840Df293413A930d3D40baB6e1Cd8F406719D";
-        deployer.deploy(Riverboat, priceReceiver).then(function(){
+        await deployer.deploy(Riverboat, priceReceiver).then(function(){
             console.log("Riverboat contract was deployed at address: "+Riverboat.address);
         });
-        deployer.deploy(RiverboatNft).then(function(){
+        await deployer.deploy(RiverboatNft).then(function(){
             console.log("RiverboatNft contract was deployed at address: "+RiverboatNft.address);
         });
-        deployer.deploy(RiverboatFactory, _RiverboatNft).then(function(){
+        await deployer.deploy(RiverboatFactory, _RiverboatNft).then(function(){
             console.log("RiverboatFactory contract was deployed at address: "+RiverboatFactory.address);
         });
     }
