36f509da521293455c4c4a3922189886dcb06225
SchneiderNejc <nejc@seascape.network>
2021-11-08 19:09:08 +0100

    update MscpVesting.sol for testing simulation in Mscp

--- contracts/mscp/MscpVesting.sol
+++ contracts/mscp/MscpVesting.sol
@@ -15,14 +15,24 @@ contract MscpVesting is Ownable {
   	address public currencyAddress;
   	uint public startTime;
 
+    uint256 private endtime_private;
+    uint256 private endtime_strategic;
+
     // constant variables
     uint256 constant private MULTIPLIER = 10**18;
     /// @dev total tokens to be released gradualy (without "day one" tokens)
-    uint256 constant private TOTAL_PRIVATE = 8500000;
-    uint256 constant private TOTAL_STRATEGIC = 8000000;
+    uint256 constant private TOTAL_PRIVATE = 8500000 * MULTIPLIER;
+    uint256 constant private TOTAL_STRATEGIC = 8000000 * MULTIPLIER;
+
+    uint256 constant private DURATION_PRIVATE = 8;
+    uint256 constant private DURATION_STRATEGIC = 8;
+
     /// @dev TPS = Tokens Per Second
-    uint256 constant private TPS_PRIVATE = MULTIPLIER * TOTAL_PRIVATE / 25920000;     /// 300 days
-    uint256 constant private TPS_STRATEGIC = MULTIPLIER * TOTAL_STRATEGIC / 12960000; /// 150 days
+    /* uint256 constant private TPS_PRIVATE = MULTIPLIER * TOTAL_PRIVATE / 25920000;     /// 300 days
+    uint256 constant private TPS_STRATEGIC = MULTIPLIER * TOTAL_STRATEGIC / 12960000; /// 150 days */
+    /// only for testing
+    uint256 constant private TPS_PRIVATE = TOTAL_PRIVATE / DURATION_PRIVATE;     /// 300 days
+    uint256 constant private TPS_STRATEGIC = TOTAL_STRATEGIC / DURATION_STRATEGIC; /// 150 days
 
     struct Balance {
         uint256 remainingCoins;
@@ -41,13 +51,19 @@ contract MscpVesting is Ownable {
 
         currencyAddress = _currencyAddress;
         startTime = _startTime;
+
+        endtime_private = startTime + DURATION_PRIVATE;
+        endtime_strategic = startTime + DURATION_STRATEGIC;
     }
 
     /// @notice add strategic investor address
     /// @param _investor address to be added
     function addStrategicInvestor (address _investor) external onlyOwner {
         require(balances[_investor].remainingCoins == 0, "investor already has allocation");
+
         balances[_investor].remainingCoins = TOTAL_STRATEGIC;
+        balances[_investor].strategicInvestor = true;
+
         emit InvestorModified(_investor, balances[_investor].remainingCoins);
     }
 
@@ -73,27 +89,39 @@ contract MscpVesting is Ownable {
         require(now >= startTime, "vesting hasnt started yet");
 
         Balance storage balance = balances[msg.sender];
-        uint256 timePassed = now - startTime;
         uint256 actualUnclaimed;
-        if(balance.strategicInvestor){
-            uint256 unclaimedPotential = (timePassed * TPS_STRATEGIC) / MULTIPLIER;
+        uint256 timePassed;
+        if(balance.strategicInvestor == true){
+            //internal getTime (seperationj within function)
+            if(now < endtime_strategic)
+                timePassed = now.sub(startTime); //remove safeMath
+            else
+                timePassed = endtime_strategic.sub(startTime);
+            //internal calculateAmount (seperation within function)
+            uint256 unclaimedPotential = (timePassed * TPS_STRATEGIC);
             actualUnclaimed = unclaimedPotential - (TOTAL_STRATEGIC - balance.remainingCoins);
             balance.remainingCoins = balance.remainingCoins.sub(actualUnclaimed);
+            //internal addBonus (seperation within function)
             if(!balance.claimedBonus) {
                 balance.claimedBonus = true;
-                actualUnclaimed = actualUnclaimed + 2000000; // 2 mil is released on day one
+                actualUnclaimed = actualUnclaimed + 2000000 * MULTIPLIER; // 2 mil is released on day one
             }
         } else {
-            uint256 unclaimedPotential = (timePassed * TPS_PRIVATE) / MULTIPLIER;
+            //internal getTime (seperationj within function)
+            if(now < endtime_private)
+                timePassed = now.sub(startTime); //remove safeMath
+            else
+                timePassed = endtime_private.sub(startTime);
+            uint256 unclaimedPotential = (timePassed * TPS_PRIVATE);
             actualUnclaimed = unclaimedPotential - (TOTAL_PRIVATE - balance.remainingCoins);
             balance.remainingCoins = balance.remainingCoins.sub(actualUnclaimed);
             if(!balance.claimedBonus) {
                 balance.claimedBonus = true;
-                actualUnclaimed = actualUnclaimed + 1500000; // 1.5 mil is released on day one
+                actualUnclaimed = actualUnclaimed + 1500000 * MULTIPLIER; // 1.5 mil is released on day one
             }
         }
-
         IERC20(currencyAddress).safeTransfer(msg.sender, actualUnclaimed);
+
         emit Withdraw(msg.sender, actualUnclaimed, balance.remainingCoins);
     }
 
@@ -107,4 +135,5 @@ contract MscpVesting is Ownable {
     function getAllocation(address _investor) public view returns(uint) {
         return balances[_investor].remainingCoins;
     }
+
 }
