c583ac68c11ecf7244f8f4629c28be33bbf6bf67
ahmetson <ahmetson@zoho.com>
2021-11-10 00:24:32 +0700

    update profit circus session starting script

--- scripts/profit_circus/init.js
+++ scripts/profit_circus/init.js
@@ -4,12 +4,6 @@ let Factory = artifacts.require("NftFactory");
 let ScapeNft = artifacts.require("SeascapeNft");
 
 let accounts;
-let reward = web3.utils.toWei("1", "ether");  // cws
-let period = 1200;
-let generation = 1;
-
-let stakeAmount = web3.utils.toWei("1", "ether"); // 1 LP token in ETHER format.
-let stakePeriod = 10;
 
 /**
  * For test purpose, starts a game session
@@ -43,34 +37,87 @@ let init = async function() {
     // let scapeNft        = await ScapeNft.at("0xB69aA417a1283618AACc0EAE123a1e01dE84f4AA");
 
     // // moonriver
-    // let profitCircus    = await ProfitCircus.at("0x805C8922b408234f5208779053D06D7E8A8d74BC").catch(console.error);
-    // let factory         = await Factory.at("0x77478212aa57A7A9Cc5b611156Fce7c0697578fb").catch(console.error);
-    // let scapeNft        = await ScapeNft.at("0x607cBd90BE76e9602548Fbe63931AbE9E8af8aA7").catch(console.error);
+    let profitCircus1         = await ProfitCircus.at("0x717d834132193FE0FDFeAcD33ed3e39C9ad51Bee").catch(console.error);
+    let factory               = await Factory.at("0x77478212aa57A7A9Cc5b611156Fce7c0697578fb").catch(console.error);
+    // let scapeNft           = await ScapeNft.at("0x607cBd90BE76e9602548Fbe63931AbE9E8af8aA7").catch(console.error);
+
+    // Binance Smart Chain
+    // let profitCircus1     = await ProfitCircus.at("0xb3f71a6d1D98F64518070f7F4493CB5C61497452").catch(console.error);
+    // let profitCircus2     = await ProfitCircus.at("0xdF704B850BFa18dCB3B92222181E5f306ac36b65").catch(console.error);
+    // let factory           = await Factory.at("0xf92186992be668093E5F158b39F88c7985108b27").catch(console.error);
+    // let scapeNft          = await ScapeNft.at("0xc54b96b04AA8828b63Cf250408E1084E9F6Ac6c8").catch(console.error);
 
     console.log("Set the contracts");
 
-    console.log("Adding factory to nft");
-    await scapeNft.setFactory(factory.address).catch(console.error);
-    console.log("Added factory to nft");
+    // uncomment the following block if you just deployed Scape NFT too.
+    // console.log("Adding factory to nft...");
+    // await scapeNft.setFactory(factory.address).catch(console.error);
+    // console.log("Added factory to nft");
 
-    console.log("adding profitCircus to factory..");
-    await factory.addStaticUser(profitCircus.address).catch(console.error);
-    console.log("Profit Circus contract got a permission to mint nfts");
+    // uncomment the following block if you just deployed Profit Circus contracts
+    console.log("adding profitCircus 1 to factory..");
+    let yes = await factory.isStaticUser(profitCircus1.address).catch(console.error);
+    console.log(yes);
+    // return;
 
+    await factory.addStaticUser(profitCircus1.address).catch(console.error);
+    console.log("Profit Circus 1 contract got a permission to mint nfts, now adding profit circus 2");
+    // await factory.addStaticUser(profitCircus2.address).catch(console.error);
+    // console.log("Profit Circus 2 contract got a permission to mint nfts!");
+    return;
+
+    // uncomment the following block if you want to transfer from the Profit Circus owner's balance the reward token
     // should transfer reward amount to contract
-    console.log("transfering reward to profitCircus..");
-    await crowns.transfer(profitCircus.address, reward, {from: accounts[0]}).catch(console.error);
-    console.log("Crowns for session were transferred to the Lp Mining smartcontract");
+    // console.log("transfering reward to profitCircus..");
+    // await crowns.transfer(profitCircus.address, reward, {from: accounts[0]}).catch(console.error);
+    // console.log("Crowns for session were transferred to the Lp Mining smartcontract");
 
-    console.log("starting the session..");
-    let startTime = Math.floor(new Date().getTime()/1000) + 30;
-    await profitCircus.startSession(crowns.address, crowns.address, reward, period, startTime, generation, stakeAmount, stakePeriod)
+    // pCWS staking test session
+    // Session time: 1 hour
+    // NFT claiming CD: 10 mins
+    // NFT claiming requirement: 100 pCWS
+    // Reward Pool: 1 pCWS
+
+    // pCWS-BNB LP staking test session
+    // Session time: 1 hour
+    // NFT claiming CD: 10 mins
+    // NFT claiming requirement: 1 pCWS-BNB LP
+    // Reward Pool: 1 pCWS
+
+    // address _rewardToken, 
+    // address _lpToken,  
+    // uint256 _totalReward, 
+    // uint256 _period,  
+    // uint256 _startTime, 
+    // uint256 _generation, 
+    // uint256 _stakeAmount, 
+    // uint256 _stakePeriod
+
+    console.log("starting the session...");
+    let rewardToken = "0xbcf39F0EDDa668C58371E519AF37CA705f2bFcbd";     // for both profit circus 1, and profit circus 2
+    let lpToken1 = "0xbcf39f0edda668c58371e519af37ca705f2bfcbd";     // pCWS
+    let lpToken2 = "0x6615ce60d71513aa4849269dd63821d324a23f8c";    // pCWS-BNB lp
+    let startTime = Math.floor(new Date().getTime()/1000) + 200;
+    let period = 7776000;                 // 90 days
+    let generation = 3;
+    let stakePeriod = 86400;    // 24 hours
+    let stakeAmount1 = web3.utils.toWei("100", "ether");
+    let stakeAmount2 = web3.utils.toWei("1", "ether");
+    let reward1 = web3.utils.toWei("10000", "ether");
+    let reward2 = web3.utils.toWei("50000", "ether");
+
+    console.log(`Start time of Season: ${startTime}...`);
+    await profitCircus1.startSession(rewardToken, lpToken1, reward1, period, startTime, generation, stakeAmount1, stakePeriod)
       .catch(console.error);
-    console.log("Session started");
+    console.log("Session started for Profit circus 1, now setting profit circus 2");
+    await profitCircus2.startSession(rewardToken, lpToken2, reward2, period, startTime, generation, stakeAmount2, stakePeriod)
+    .catch(console.error);
+    console.log("Session started for Profit circus 2");
 
-    console.log("fetching sessionId..");
-    let sessionId = await profitCircus.lastSessionIds.call(crowns.address);
-    console.log(sessionId +" session id for "+crowns.address);
+    // uncomment the block if you want to check the Session ID
+    // console.log("fetching sessionId..");
+    // let sessionId = await profitCircus.lastSessionIds.call(crowns.address);
+    // console.log(sessionId +" session id for "+crowns.address);
 
     return true;
 }.bind(this);
