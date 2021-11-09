db75b5f46738f77537669b4d574a3c0a25395656
ahmetson <ahmetson@zoho.com>
2021-11-10 00:24:22 +0700

    Update the minting nfts script

--- scripts/nft/mint.js
+++ scripts/nft/mint.js
@@ -2,8 +2,11 @@ let Factory = artifacts.require("NftFactory");
 let Nft = artifacts.require("SeascapeNft");
 
 // rinkeby testnet
-let factoryAddress = '0x3eB88c3F2A719369320D731FbaE062b0f82F22e4';
-let nftAddress = '0x66638F4970C2ae63773946906922c07a583b6069';
+// let factoryAddress = '0x3eB88c3F2A719369320D731FbaE062b0f82F22e4';
+// let nftAddress = '0x66638F4970C2ae63773946906922c07a583b6069';
+
+// moonriver
+let factoryAddress = "0x77478212aa57A7A9Cc5b611156Fce7c0697578fb";
 
 module.exports = async function(callback) {
     const networkId = await web3.eth.net.getId();
@@ -19,45 +22,36 @@ let max = 5;
 let grantPermission = async function(factory, address) {
     let res = await factory.addGenerator(address);
     console.log(res);
-    console.log(`Account ${address} granted a GENERATOR permission in Nft Factory`);
+    console.log(`Account ${address} was granted a GENERATOR role in Nft Factory`);
     return res;
 }.bind(this);
 
 let init = async function(networkId) {
     accounts = await web3.eth.getAccounts();
-    console.log(accounts);
+    console.log(`Minting account ${accounts[0]}`);
 
     let factory;
     let nft;
 
-        factory = await Factory.deployed();
-        nft = await Nft.deployed();
+    factory = await Factory.at(factoryAddress);
+    // nft = await Nft.at();
 
     console.log("Contracts initiated")
 
-    await grantPermission(factory, accounts[0]);
-
-    console.log("Got a permission")
-
-    let amount = 5;
-    let args = process.argv.slice(4);
-    if (args.length == 1) {
-	    amount = parseInt(args[0]);
-	    if (amount < min || amount > max) {
-	        throw "Number of minting NFTs should be between 1 and 5";
-	    }
-    }
-    
+    // Uncomment the code line below to give permission to mint
+    // await grantPermission(factory, accounts[0]);
 
     console.log("Set arguments")
 
-    let owner = accounts[0];
-    let generation = 0;
+    let owner = "0xAfD8B531CAD0393Fe4137634b0e71C1016CFe838";
+    let generation = 2;
+    let quality = 2;
+    let amount = 1;
 
-    for (var quality = 1; quality <= amount; quality++) {
-        let res = await factory.mintQuality(owner, generation, 4, {from: accounts[0], gasPrice: 136000000000});
+    for (var i = 0; i < amount; i++) {
+        let res = await factory.mintQuality(owner, generation, quality, {from: accounts[0]}).catch(console.error);
         console.log("-------------------------");
-        console.log(`Quality ${4} was minted!`);	
+        console.log(`NFT ${i}/${amount} of quality ${quality} was minted for ${owner}!`);	
         console.log(`Txid: ${res.tx}`);
         console.log();
     }
