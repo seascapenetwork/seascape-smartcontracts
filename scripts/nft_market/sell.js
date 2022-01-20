78af16bd7e48bf237f7da4285bb4fb449d0f41b8
SchneiderNejc <nejc@seascape.network>
2022-01-20 22:21:49 +0100

    update sell.js script in nft_market

--- scripts/nft_market/sell.js
+++ scripts/nft_market/sell.js
@@ -15,26 +15,29 @@ let init = async function(networkId) {
     accounts = await web3.eth.getAccounts();
     console.log(accounts);
 
-    let nftMarket = await NftMarket.at("0xbB8b7bF5CAd05829DC99eEFF469C9431CA2F75DF");
-    let nft     = await Nft.at("0x66638F4970C2ae63773946906922c07a583b6069");
-    let crowns  = await Crowns.at("0x4Ca0ACab9f6B9C084d216F40963c070Eef95033B");
+    let nftMarket = await NftMarket.at("0x93A782FD81444dC3fE2EFB529Dd7a6FB9eef7190");
+    let nft     = await Nft.at("0x14C7C9D806c7fd8c1B45d466B910c6AbF6428F07");
+    let crowns  = await Crowns.at("0xFde9cad69E98b3Cc8C998a8F2094293cb0bD6911");
 
 
     let user = accounts[0];
     console.log(`Using ${user}`);
 
     // fetch nftId
-    let nftId = await nft.tokenOfOwnerByIndex(user, 0)
-      .catch(console.error);
-    console.log(`First nft of ${user} is nft id ${nftId}`);
+    let nftId = 39;
+    // let nftId = await nft.tokenOfOwnerByIndex(user, 0)
+    //   .catch(console.error);
+    // console.log(`First nft of ${user} is nft id ${nftId}`);
 
     // approve transfer of nft
+    console.log("attempting to approve nftMarket to spend nfts...")
     await nft.setApprovalForAll(nftMarket.address, true, {from: user})
       .catch(console.error);
     console.log("nftMarket was approved to spend nfts")
 
     //put nft for sale
     let price = web3.utils.toWei("1", "ether");
+    console.log("attempting to put nft for sale...")
     let onSale = await nftMarket.sell(nftId, price, nft.address, crowns.address, {from: user})
       .catch(console.error);
     console.log(onSale.tx);
