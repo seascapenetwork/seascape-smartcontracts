316027ef88407a3614473d87a8cd0c9bb775f874
SchneiderNejc <nejc@seascape.network>
2022-01-25 21:02:53 +0100

    update init.js in nft_market

--- scripts/nft_market/init.js
+++ scripts/nft_market/init.js
@@ -1,6 +1,4 @@
 let NftMarket = artifacts.require("NftMarket");
-let Crowns = artifacts.require("CrownsToken");
-let Nft = artifacts.require("SeascapeNft");
 
 
 let accounts;
@@ -15,26 +13,28 @@ let init = async function(networkId) {
     accounts = await web3.eth.getAccounts();
     console.log(accounts);
 
-    let nftMarket = await NftMarket.at("0xF4C4Ea5508Fe0EEb3E2f300AB8AfFDa1881cf96a");
-    let nft     = await Nft.at("0x7115ABcCa5f0702E177f172C1c14b3F686d6A63a");
-    let crowns  = await Crowns.at("0x168840Df293413A930d3D40baB6e1Cd8F406719D");
+    let nftMarket = await NftMarket.at("0x1af963a8bD09F13F412b967dcEB987d64a11dF05");
 
 
     let user = accounts[0];
     console.log(`Using ${user}`);
 
+    let currencyAddress = "0x4bDE98731149093a12579D71338fd3741fe6E5Ce";
+    let nft = "0x7cDc5D0188733eDF08412EECb9AFa840772615dC";
+
     // enable sales (onlyOwner) -only needs to run once
-    let salesStarted = await nftMarket.enableSales(true, {from: user});
-    console.log(`salesEnabled was set`);
+    // let salesStarted = await nftMarket.enableSales(true, {from: user});
+    // console.log(`salesEnabled was set`);
 
     // add nft address -only needs to run once per nft
-    let nftAddressAdded = await nftMarket.addSupportedNft(nft.address, {from: user})
+    console.log("attempting to add nft address...");
+    let nftAddressAdded = await nftMarket.addSupportedNft(nft, {from: user})
       .catch(console.error);
     console.log("nft address added");
 
     // add currency address -only needs to run once per currency
-    let currencyAddressAdded = await nftMarket.addSupportedCurrency(crowns.address, {from: user})
-      .catch(console.error);
-    console.log("currency address added");
+    // console.log("attempting to add currency...");
+    // let currencyAddressAdded = await nftMarket.addSupportedCurrency(currencyAddress).catch(console.error);
+    // console.log("currency address added");
 
 }.bind(this);
