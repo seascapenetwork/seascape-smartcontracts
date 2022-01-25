70a1b68539ccde82c5263fcdbd921699fe63df33
SchneiderNejc <nejc@seascape.network>
2022-01-25 21:18:31 +0100

    update init.js in riverboat

--- scripts/riverboat/init.js
+++ scripts/riverboat/init.js
@@ -1,7 +1,4 @@
-let Riverboat = artifacts.require("Riverboat");
-let RiverboatNft = artifacts.require("RiverboatNft");
-let RiverboaFactory = artifacts.require("RiverboaFactory");
-
+let CrownsToken = artifacts.require("CrownsToken");
 
 // global variables
 let accounts;
@@ -22,11 +19,9 @@ let init = async function(networkId) {
     accounts = await web3.eth.getAccounts();
     console.log(accounts);
 
-    let riverboat = await Riverboat.at("0x8E61f5028eEA48fdd58FD3809fc2202ABdBDC126");
-    let riverboatNft     = await RiverboatNft.at("0x7115ABcCa5f0702E177f172C1c14b3F686d6A63a");
-    let riverboaFactory = await RiverboaFactory.at("0x8BDc19BAb95253B5B30D16B9a28E70bAf9e0101A");
+    let mscpToken = await CrownsToken.at("0x1aBB8FdE5e64be3419FceF80df335C68dD2956C7");
 
-
+    let bridge = "0x911F32FD5d347b4EEB61fDb80d9F1063Be1E78E6";
     let owner = accounts[0];
     console.log(`Using account ${owner}`);
 
@@ -34,36 +29,19 @@ let init = async function(networkId) {
     // Parameters setup and function calls
     //--------------------------------------------------
 
-    let riverboatNftAddress = riverboatNft.address;
-    let nftMetadataAddress = riverboaFactory.address;
-
-    let tradeEnabled = true;
-    let priceReceiver = owner;
-
-
+    let investorAddress = "";
 
     // contract calls
-    await enableTrade();
-    await setPriceReceiver();
-    // NOTE add factory calls
+    await addBridge();
 
-    //--------------------------------------------------
-    // Functions operating the contract
-    //--------------------------------------------------
 
-    // add currency address -only needs to run once per currency
-    async function setPriceReceiver(){
-        console.log("attempting to set price receiver...");
-        await riverboat.setPriceReceiver(priceReceiver, {from: owner})
-          .catch(console.error);
-        console.log(`${currencyAddress} was set as price receiver`);
-    }
+    async function addBridge(){
+      console.log("attempting to add bridge...");
+      await mscpToken.addBridge(bridge);
+      console.log(`bridge added`);
 
-    // enable trade (true/false) -only needs to run once
-    async function enableTrade(){
-      console.log("attempting to enable trade...");
-      await riverboat.enableTrade(tradeEnabled, {from: owner});
-      console.log(`tradeEnabled was set to ${tradeEnabled}`);
+      let isBuyer = await mscpToken.bridges(bridge);
+      console.log(isBuyer);
     }
 
 
