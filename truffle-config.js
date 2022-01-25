86373d7c74daf9f9a97f49f6660af7d412d74259
SchneiderNejc <nejc@seascape.network>
2022-01-25 21:03:39 +0100

    update networks in truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -30,22 +30,46 @@ module.exports = {
 	    // https://github.com/trufflesuite/truffle/issues/3008
 	},
   bsctestnet: {
-    provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://data-seed-prebsc-2-s1.binance.org:8545/`),
+    provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://data-seed-prebsc-1-s1.binance.org:8545/`, 0, 5),
     network_id: 97,
     confirmations: 10,
     timeoutBlocks: 200,
     skipDryRun: true
   },
   // Moonbase Alpha TestNet
-  moonbase: {
-    provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://rpc.testnet.moonbeam.network`),
+  moonbase: {                 // alternative RPCs
+    // https://moonbeam-alpha.api.onfinality.io/rpc?apikey=cb6ddff3-c8b5-491f-a4ac-9ac1a12daeb2
+    // https://moonbeam-alpha.api.onfinality.io/public
+    // https://rpc.api.moonbase.moonbeam.network
+    provider: () => new HDWalletProvider(process.env.MNEMONIC,
+      `https://rpc.api.moonbase.moonbeam.network`, 0, 5),
     network_id: 1287,
     gas: 5190000
-},
+  },
+  // alternative  https://rpc.moonriver.moonbeam.network
+  // https://moonriver.api.onfinality.io/public
+  moonriver: {
+    provider: () => new HDWalletProvider(process.env.MNEMONIC, 'https://rpc.moonriver.moonbeam.network', 0, 5),
+    network_id: 1285
+    //gasPrice: 20000000000
+  },
+  moonbeam: {
+    provider: () => new HDWalletProvider(process.env.MNEMONIC, 'https://rpc.api.moonbeam.network', 0, 5),
+    network_id: 1284,
+    //gas: 5190000,
+    gasPrice: 100000000000
+  },
+  bsc: {
+    provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://bsc-dataseed.binance.org/`, 0, 5),
+    network_id: 56,
+    confirmations: 10,
+    timeoutBlocks: 200,
+    skipDryRun: true
+  },
   ganache: {
     host: "localhost",
     port: 9545,
     network_id: "*"
-}
+  }
 }
 };
