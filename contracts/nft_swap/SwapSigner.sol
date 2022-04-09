41808c2aa4cfec18a579792ce8d029cc23269c29
Nejc Schneider <nejc@gmail.com>
2022-04-09 13:47:52 -0600

    add SwapSigner.sol in Nft Swap

---
+++ contracts/nft_swap/SwapSigner.sol
@@ -0,0 +1,24 @@
+pragma solidity 0.6.7;
+
+import "./../openzeppelin/contracts/access/Ownable.sol";
+
+/// @title SwapSigner holds address for signature verification.
+/// It is used by NftSwap and SwapParams contracts.
+/// @author Nejc Schneider
+contract SwapSigner is Ownable {
+
+    address public signer;         // @dev verify v, r, s signature
+
+    constructor() public { signer = msg.sender; }
+
+    /// @notice change address to verify signature against
+    /// @param _signer new signer address
+    function setSigner(address _signer) external onlyOwner {
+        require(_signer != address(0), "invalid signer address");
+        signer = _signer;
+    }
+
+    /// @notice returns verifier of signatures
+    /// @return signer address
+    function getSigner() external view returns(address) { return signer; }
+}
