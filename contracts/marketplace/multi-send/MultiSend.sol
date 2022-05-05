2305e92b4b3e3531b3649de21a8e12de385fc8d9
SchneiderNejc <nejc@seascape.network>
2022-05-05 15:23:18 +0200

    update reason strings and indentation in MultiSend.sol

--- contracts/marketplace/multi-send/MultiSend.sol
+++ contracts/marketplace/multi-send/MultiSend.sol
@@ -1,26 +1,29 @@
 pragma solidity 0.6.7;
-import "./../openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+import "./../../openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 /// @title Multi send is a part of Seascape marketplace platform.
 /// It allows users to send a batch of nfts to desired address
 /// @author Nejc Schneider
 contract MultiSend {
-    function sendNfts(uint amount, address receiver, address nftAddresses[], uint nftIds[])
+    function sendNfts(
+        uint amount,
+        address receiver,
+        address[] calldata nftAddresses,
+        uint[] calldata nftIds
+    )
         external
         returns(uint)
     {
-        require(amount > 1, "minimum 2 nfts are required.");
-        require(amount < 10, "maximum 10 nfts are allowed.");
+        require(amount > 1, "minimum 2 nfts are required");
+        require(amount < 11, "maximum 10 nfts are allowed");  /// set max amount here
         for(uint i = 0; i < amount; ++i){
-          IERC721 nft = IERC721(_offeredTokens[index].tokenAddress);
-          require(nft.ownerOf(_offeredTokens[index].tokenId) == msg.sender,
+            require(IERC721(nftAddresses[i]).ownerOf(nftIds[i]) == msg.sender,
               "sender not owner of nft");
         }
         for(uint i = 0; i < amount; ++i){
-          IERC721(obj.offeredTokens[index].tokenAddress)
-              .safeTransferFrom(address(this), msg.sender, obj.offeredTokens[index].tokenId);
+            IERC721(nftAddresses[i]).safeTransferFrom(msg.sender, receiver, nftIds[i]);
         }
-
-
         return amount;
+    }
 }
