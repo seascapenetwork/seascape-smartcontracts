426f4a5a7c1c9a75fd90b96597f4d80bbc08cb38
SchneiderNejc <nejc@seascape.network>
2022-05-11 00:24:42 +0200

    add multi_send.js script in Multi Send

---
+++ scripts/marketplace/multi_send/multi_send.js
@@ -0,0 +1,82 @@
+let Nft = artifacts.require("ERC721");
+let MultiSend = artifacts.require("MultiSend");
+
+let accounts;
+
+module.exports = async function(callback) {
+    const networkId = await web3.eth.net.getId();
+    let res = await init(networkId);
+    callback(null, res);
+};
+
+let init = async function(networkId) {
+    accounts = await web3.eth.getAccounts();
+    console.log(accounts);
+
+    //--------------------------------------------------
+    // Contracts setup
+    //--------------------------------------------------
+
+    console.log("passing");
+    let nft = await Nft.at("0xb807A3F0AF8dD50725DB01d0723930bB0DB9837f");
+    let multiSend = await MultiSend.at("0xD9B9dC9fE2DEfc87f8d198b25cC8A068Ca45f330");
+    console.log("passing");
+    //--------------------------------------------------
+    // Parameters setup
+    //--------------------------------------------------
+
+    let sender = accounts[0];
+    let receiver = accounts[1];
+    let amount = 2;
+
+    // NOTE if sending nfts with same address (recommended),
+    // dont change the following values
+    let nftIds = await getNftIds(sender, nft, amount);
+    console.log("passing");
+    let nftAddresses = new Array(amount).fill(nft);
+    console.log("passing");
+
+    //--------------------------------------------------
+    // Function calls
+    //--------------------------------------------------
+
+    await getNftBalance(nft, sender);
+    await getNftBalance(nft, receiver);
+
+    await approveNfts(multiSend.address);
+    await sendNfts(amount, receiver, nftAddresses, nftIds, sender);
+
+    // --------------------------------------------------
+    // External function calls
+    // --------------------------------------------------
+
+    async function getNftBalance(token, owner){
+      console.log("Checking nft balance...");
+      let balance = parseInt(await token.balanceOf(owner));
+      console.log(`${owner} owns ${balance} nfts`);
+    }
+
+    async function getNftIds(user, token, amount){
+      let nftIds = [];
+      for(let index = 0; index < amount; index++){
+        console.log("fetching..");
+        let tokenId = await token.tokenOfOwnerByIndex(user, index);
+        nftIds[index] = parseInt(tokenId);
+      }
+      return nftIds;
+    }
+
+    async function setApprovalForAll(operator){
+      console.log("attempting to set approval for all...");
+      let approved = await erc721.setApprovalForAll(operator, true, {from: sender}).catch(console.error);
+      console.log(`${operator} was approved to transfer all tokens from ${sender} ? ${approved}`);
+    }
+
+    async function sendNfts(amount, receiver, nftAddresses, nftIds, sender){
+      console.log("attempting to multi send...");
+      await multiSend.sendNfts(amount, receiver, nftAddresses, nftIds, {from: sender}).catch(console.error);
+      console.log(`${amount} nfts were transfered from ${sender} to ${receiver}`);
+    }
+
+
+}.bind(this);
