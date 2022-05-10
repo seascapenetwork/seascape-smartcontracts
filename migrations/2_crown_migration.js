a508a72c4f8e0a60f80fd7211a7833d44f55376c
SchneiderNejc <nejc@seascape.network>
2022-05-10 15:43:18 +0200

    remove all the contracts running on old version

--- migrations/2_crown_migration.js
+++
@@ -1,9 +0,0 @@
-var Crowns = artifacts.require("./CrownsToken.sol");
-
-let type = 1;
-
-module.exports = function(deployer, network) {
-    deployer.deploy(Crowns, type).then(function(){
-	    console.log("Crowns deployed on "+Crowns.address);
-    });
-}
