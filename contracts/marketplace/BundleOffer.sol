13dc40badbb73133f12b1cab2427eb9864817552
SchneiderNejc <nejc.sch@gmail.com>
2022-06-17 18:04:21 +0200

    remove getOffer method in BundleOffer.sol

--- contracts/marketplace/BundleOffer.sol
+++ contracts/marketplace/BundleOffer.sol
@@ -124,23 +124,6 @@ contract BundleOffer is IERC721Receiver, Ownable {
     // external methods
     //--------------------------------------------------
 
-    /// @dev returns all properties of sale object at _saleId element
-    /// @return elements of SaleObject at given saleId
-    function getOffer(uint _saleId)
-        external
-        view
-        returns(uint, uint, uint, address, address)
-    {
-        return
-        (
-            salesObjects[_saleId].saleId,
-            salesObjects[_saleId].price,
-            salesObjects[_saleId].nftsAmount,
-            salesObjects[_saleId].seller,
-            salesObjects[_saleId].currency
-        );
-    }
-
     /// @notice cancel nft sale
     function cancelSale(uint _saleId) external {
         SalesObject storage offer = salesObjects[_saleId];
