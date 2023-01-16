c4c22ef534710654c8bbd74dd093652ef480f790
ahmetson <ahmetson@zoho.com>
2023-01-16 19:41:20 +0300

    Update sample truffle-config.json (rename and insert private keys to begin your Seascape Journey :D )

--- sample-truffle-config.js
+++ sample-truffle-config.js
@@ -1,27 +1,40 @@
 let HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
 
+// Add the private key, otherwise truffle framework will yell at you.
 module.exports = {
     networks: {
       development: {
         host: "local-node",
         port: 8545,
-        network_id: "*" // Match any network id
+        network_id: process.env.NETWORK_ID // Match any network id
       },
       moonriver: {
         provider: function() {
           return new HDWalletProvider({
-              privateKeys: [process.env.ACCOUNT_1],
+              privateKeys: [""],
               providerOrUrl: "https://rpc.moonriver.moonbeam.network",
               addressIndex: 0
           })
         },
         network_id: 1285
       },
-      rinkeby: {
+      bsc: {
         provider: function() {
           return new HDWalletProvider({
-            privateKeys: [process.env.ACCOUNT_1],
-            providerOrUrl: process.env.REMOTE_HTTP,
+              privateKeys: [""],
+              providerOrUrl: "https://bsc-dataseed4.defibit.io",
+              addressIndex: 0
+          })
+        },
+        network_id: 56
+      },
+      sepolia: {
+        network_id: 11155111,
+        provider: function() {
+          return new HDWalletProvider({
+            privateKeys: [""],
+            providerOrUrl: "https://rpc.bordel.wtf/sepolia",
             addressIndex: 0
           })
         }
@@ -29,7 +42,7 @@ module.exports = {
     },
     compilers: {
       solc: {
-        version: "0.6.7"
+        version: "0.6.7",
       }
     }
   };
