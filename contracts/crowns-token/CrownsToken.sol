3a9608fb4b34a05987fd33948f8bef0c388423f7
SeasonLei <30384084@qq.com>
2023-07-21 16:18:51 +0800

    cws:Change the name of the newcrownstoken contract to crownstoken and remove the unused MIN_SPEND variable

---
+++ contracts/crowns-token/CrownsToken.sol
@@ -0,0 +1,326 @@
+// contracts/Crowns.sol
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.7;
+
+import "./../openzeppelin/contracts/access/Ownable.sol";
+import "./../openzeppelin/contracts/GSN/Context.sol";
+import "./../openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./../openzeppelin/contracts/math/SafeMath.sol";
+import "./../openzeppelin/contracts/utils/Address.sol";
+
+/// @title Official token of the Seascape ecosystem.
+/// to be claimed by users by triggering any transaction in the ERC-20 contract.
+/// @dev Implementation of the {IERC20} interface.
+contract CrownsToken is Context, IERC20, Ownable {
+    using SafeMath for uint256;
+    using Address for address;
+
+    struct Account {
+        uint256 balance;
+    }
+
+    mapping (address => Account) private _accounts;
+    mapping (address => mapping (address => uint256)) private _allowances;
+
+    uint256 private _totalSupply;
+
+    string private constant _name = "Crowns";
+    string private constant _symbol = "CWS";
+    uint8 private immutable _decimals = 18;
+
+    uint256 private constant SCALER = 10 ** 18;
+
+    /**
+     * @dev Sets the {name} and {symbol} of token.
+     * Initializes {decimals} with a default value of 18.
+     * Mints all tokens.
+     * Transfers ownership to another account. So, the token creator will not be counted as an owner.
+     */
+    constructor () public {
+        // Multi-sig wallet accounts to hold the pools and ownership.
+        address gameIncentivesHolder = 0x37fa6cBFA07D6598ac902Ed059D793d3f6ef0818;
+        address liquidityHolder      = 0xA65933EE0123DF03216FdfD9EedD473F25085594;
+        address teamHolder           = 0x98e19EfC8843b38ff3Ba644C588fD67FB89D8912;
+        address investHolder         = 0x68bA259675C9B6eCbC5E4275521FB12AE6356e19;
+        address communityHolder      = 0x15b63CBDc7F324dfD8B3d63d2950939410A4767D;
+        address newOwner             = 0x9B705E242bB64A870B08a4889182647c749b382b;
+
+        // 1.9million tokens
+        uint256 gameIncentives       = 19e5 * SCALER;
+        // 1.5 million tokens
+        uint256 reserve              = 15e5 * SCALER; // reserve for the next 5 years.
+        // 300,000 tokens
+        uint256 community            = 3e5 * SCALER;
+        uint256 team                 = 8e5 * SCALER;
+        uint256 investment           = 5e6 * SCALER;
+        // 500,000 tokens
+        uint256 liquidity            = 5e5 * SCALER;
+
+        _mint(gameIncentivesHolder,  gameIncentives);
+        _mint(liquidityHolder,       liquidity);
+        _mint(teamHolder,            team);
+        _mint(investHolder,          investment);
+        _mint(communityHolder,       community);
+        _mint(newOwner,              reserve);
+
+        transferOwnership(newOwner);
+    }
+
+
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() public pure returns (string memory) {
+        return _name;
+    }
+
+    /**
+     * @dev Returns the symbol of the token, usually a shorter version of the
+     * name.
+     */
+    function symbol() public pure returns (string memory) {
+        return _symbol;
+    }
+
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5,05` (`505 / 10 ** 2`).
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract, including
+     * {IERC20-balanceOf} and {IERC20-transfer}.
+     */
+    function decimals() public view override returns (uint8) {
+        return _decimals;
+    }
+
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() public view override returns (uint256) {
+        return _totalSupply;
+    }
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) public view override returns (uint256) {
+        return _getBalance(account);
+    }
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) public virtual override returns (bool) {
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
+        _transfer(sender, recipient, amount);
+        _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "ERC20: transfer amount exceeds allowance"));
+        return true;
+    }
+
+    /**
+     * @dev Atomically increases the allowance granted to `spender` by the caller.
+     *
+     * This is an alternative to {approve} that can be used as a mitigation for
+     * problems described in {IERC20-approve}.
+     *
+     * Emits an {Approval} event indicating the updated allowance.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     */
+    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].add(addedValue));
+        return true;
+    }
+
+    /**
+     * @dev Atomically decreases the allowance granted to `spender` by the caller.
+     *
+     * This is an alternative to {approve} that can be used as a mitigation for
+     * problems described in {IERC20-approve}.
+     *
+     * Emits an {Approval} event indicating the updated allowance.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     * - `spender` must have allowance for the caller of at least
+     * `subtractedValue`.
+     */
+    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].sub(subtractedValue, "ERC20: decreased allowance below zero"));
+        return true;
+    }
+
+    /**
+     * @dev Moves tokens `amount` from `sender` to `recipient`.
+     *
+     * This is internal function is equivalent to {transfer}, and can be used to
+     * e.g. implement automatic token fees, slashing mechanisms, etc.
+     *
+     * Emits a {Transfer} event.
+     *
+     * Requirements:
+     *
+     * - `sender` cannot be the zero address.
+     * - `recipient` cannot be the zero address.
+     * - `sender` must have a balance of at least `amount`.
+     */
+    function _transfer(address sender, address recipient, uint256 amount) internal virtual {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(amount > 0, "Can not send 0 token");
+        require(_getBalance(sender) >= amount, "ERC20: Not enough token to send");
+
+        _beforeTokenTransfer(sender, recipient, amount);
+
+        _accounts[sender].balance =  _accounts[sender].balance.sub(amount);
+        _accounts[recipient].balance = _accounts[recipient].balance.add(amount);
+
+        emit Transfer(sender, recipient, amount);
+    }
+
+    /** @dev Creates `amount` tokens and assigns them to `account`, increasing
+     * the total supply.
+     *
+     * Emits a {Transfer} event with `from` set to the zero address.
+     *
+     * Requirements
+     *
+     * - `to` cannot be the zero address.
+     */
+    function _mint(address account, uint256 amount) internal virtual {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        _beforeTokenTransfer(address(0), account, amount);
+
+        _totalSupply = _totalSupply.add(amount);
+        _accounts[account].balance = _accounts[account].balance.add(amount);
+        emit Transfer(address(0), account, amount);
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from `account`, reducing the
+     * total supply.
+     *
+     * Emits a {Transfer} event with `to` set to the zero address.
+     *
+     * Requirements:
+     *
+     * - `account` cannot be the zero address.
+     * - `account` must have at least `amount` tokens.
+     */
+    function _burn(address account, uint256 amount) internal virtual {
+        require(account != address(0), "ERC20: burn from the zero address");
+
+        _beforeTokenTransfer(account, address(0), amount);
+
+        uint256 accountBalance = _accounts[account].balance;
+        require(accountBalance >= amount, "ERC20: burn amount exceeds balance");
+        _accounts[account].balance = accountBalance.sub(amount);
+        _totalSupply = _totalSupply.sub(amount);
+
+        emit Transfer(account, address(0), amount);
+    }
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the `owner`s tokens.
+     *
+     * This is internal function is equivalent to `approve`, and can be used to
+     * e.g. set automatic allowances for certain subsystems, etc.
+     *
+     * Emits an {Approval} event.
+     *
+     * Requirements:
+     *
+     * - `owner` cannot be the zero address.
+     * - `spender` cannot be the zero address.
+     */
+    function _approve(address owner, address spender, uint256 amount) internal virtual {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    /**
+     * @dev Hook that is called before any transfer of tokens. This includes
+     * minting and burning.
+     *
+     * Calling conditions:
+     *
+     * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
+     * will be to transferred to `to`.
+     * - when `from` is zero, `amount` tokens will be minted for `to`.
+     * - when `to` is zero, `amount` of ``from``'s tokens will be burned.
+     * - `from` and `to` are never both zero.
+     *
+     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
+     */
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual { }
+
+    /**
+     * @dev Returns actual balance of account as a sum of owned divends and current balance.
+     * @param account Address of Token holder.
+     * @return Token amount
+     */
+    function _getBalance(address account) private view returns (uint256) {
+        uint256 balance = _accounts[account].balance;
+        if (balance == 0) {
+            return 0;
+        }
+
+        return balance;
+    }
+}
